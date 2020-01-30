ac81d040535eaf5c231e64d53173f2554d992916
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-01-30 17:12:27 +0100

    ci: Bump sdk orb (#206)

--- internal/httpclient/models/login_request_method.go
+++ internal/httpclient/models/login_request_method.go
@@ -12,7 +12,7 @@ import (
 	"github.com/go-openapi/swag"
 )
 
-// LoginRequestMethod LoginRequestMethod login request method
+// LoginRequestMethod login request method
 // swagger:model loginRequestMethod
 type LoginRequestMethod struct {
 
