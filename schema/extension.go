fe85c217631442823c9782af23a516dc2274b5b4
aeneasr <aeneas@ory.sh>
2020-02-05 13:35:47 +0100

    feat: Move everything to ory/jsonschema/v3

---
+++ schema/extension.go
@@ -0,0 +1,110 @@
+package schema
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/gobuffalo/packr/v2"
+	"github.com/ory/jsonschema/v3"
+	"github.com/pkg/errors"
+)
+
+var box = packr.New("contrib", "contrib")
+
+const (
+	ExtensionRunnerIdentityMetaSchema ExtensionRunnerMetaSchema = "extensions/identity.json"
+	ExtensionRunnerOIDCMetaSchema     ExtensionRunnerMetaSchema = "extensions/oidc.json"
+	extensionName                                               = "ory.sh/kratos"
+)
+
+type (
+	ExtensionRunnerMetaSchema string
+	ExtensionConfig           struct {
+		Credentials struct {
+			Password struct {
+				Identifier bool `json:"identifier"`
+			} `json:"password"`
+		} `json:"credentials"`
+		Mappings struct {
+			Identity struct {
+				Traits []struct {
+					Path string `json:"path"`
+				} `json:"traits"`
+			} `json:"identity"`
+		} `json:"mappings"`
+	}
+	runner          func(ctx jsonschema.ValidationContext, config ExtensionConfig, value interface{}) error
+	ExtensionRunner struct {
+		meta     *jsonschema.Schema
+		compile  func(ctx jsonschema.CompilerContext, m map[string]interface{}) (interface{}, error)
+		validate func(ctx jsonschema.ValidationContext, s interface{}, v interface{}) error
+
+		runners []runner
+	}
+)
+
+func NewExtensionRunner(meta ExtensionRunnerMetaSchema, runners ...runner) (*ExtensionRunner, error) {
+	var err error
+	schema, err := box.FindString(string(meta))
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	r := new(ExtensionRunner)
+	r.meta, err = jsonschema.CompileString(string(meta), schema)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	r.compile = func(ctx jsonschema.CompilerContext, m map[string]interface{}) (interface{}, error) {
+		if raw, ok := m[extensionName]; ok {
+			var b bytes.Buffer
+			if err := json.NewEncoder(&b).Encode(raw); err != nil {
+				return nil, errors.WithStack(err)
+			}
+
+			var e ExtensionConfig
+			if err := json.NewDecoder(&b).Decode(&e); err != nil {
+				return nil, errors.WithStack(err)
+			}
+
+			return &e, nil
+		}
+		return nil, nil
+	}
+
+	r.validate = func(ctx jsonschema.ValidationContext, s interface{}, v interface{}) error {
+		c, ok := s.(*ExtensionConfig)
+		if !ok {
+			return nil
+		}
+
+		for _, runner := range r.runners {
+			if err := runner(ctx, *c, v); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	r.runners = runners
+	return r, nil
+}
+
+func (r *ExtensionRunner) Register(compiler *jsonschema.Compiler) *ExtensionRunner {
+	compiler.Extensions[extensionName] = r.Extension()
+	return r
+}
+
+func (r *ExtensionRunner) Extension() jsonschema.Extension {
+	return jsonschema.Extension{
+		Meta:     r.meta,
+		Compile:  r.compile,
+		Validate: r.validate,
+	}
+}
+
+func (r *ExtensionRunner) AddRunner(run runner) *ExtensionRunner {
+	r.runners = append(r.runners, run)
+	return r
+}
---
+++ schema/extension_test.go
@@ -0,0 +1,53 @@
+package schema
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/ory/jsonschema/v3"
+	_ "github.com/ory/jsonschema/v3/fileloader"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExtensionRunner(t *testing.T) {
+	for k, tc := range []struct {
+		expectErr error
+		schema    string
+		doc       string
+		expect    []string
+	}{
+		{
+			doc:    `{"email":"foo@ory.sh"}`,
+			schema: "file://./stub/extensions/identity_traits/schema.json",
+			expect: []string{"foo@ory.sh"},
+		},
+		{
+			doc:    `{"emails":["foo@ory.sh","bar@ory.sh"]}`,
+			schema: "file://./stub/extensions/identity_traits/schema.nested.json",
+			expect: []string{"foo@ory.sh","bar@ory.sh"},
+		},
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			c := jsonschema.NewCompiler()
+			runner, err := NewExtensionRunner(ExtensionRunnerIdentityMetaSchema)
+			require.NoError(t, err)
+
+			var identifiers []string
+			runner.AddRunner(func(ctx jsonschema.ValidationContext, config ExtensionConfig, value interface{}) error {
+				if config.Credentials.Password.Identifier {
+					identifiers = append(identifiers, fmt.Sprintf("%s", value))
+				}
+				return nil
+			}).Register(c)
+
+			err = c.MustCompile(tc.schema).Validate(bytes.NewBufferString(tc.doc))
+			if tc.expectErr != nil {
+				require.EqualError(t, err, tc.expectErr.Error())
+			}
+
+			assert.EqualValues(t, tc.expect, identifiers)
+		})
+	}
+}
