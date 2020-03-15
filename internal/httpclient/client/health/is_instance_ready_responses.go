5f94c7221eb7bb2a0a12b7c22f394b64586b58d3
aeneasr <aeneas@ory.sh>
2020-03-15 14:05:35 +0000

    chore: Regenerate swagger spec and internal client

--- internal/httpclient/client/health/is_instance_ready_responses.go
+++ internal/httpclient/client/health/is_instance_ready_responses.go
@@ -10,8 +10,7 @@ import (
 	"io"
 
 	"github.com/go-openapi/runtime"
-
-	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/strfmt"
 
 	"github.com/ory/kratos/internal/httpclient/models"
 )
--- internal/httpclient/models/generic_error_payload.go
+++ internal/httpclient/models/generic_error_payload.go
@@ -6,11 +6,12 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
 
 // GenericErrorPayload nolint:deadcode,unused
+//
 // swagger:model genericErrorPayload
 type GenericErrorPayload struct {
 
