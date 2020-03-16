1935510ad9b0f387eb3b2e690e31c5313a06883e
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-03-16 12:14:46 +0100

    fix(swagger): Move nolint,deadcode instructions to own file (#293)
    Closes https://github.com/ory/docs/pull/279

--- internal/httpclient/client/admin/list_identities_responses.go
+++ internal/httpclient/client/admin/list_identities_responses.go
@@ -49,8 +49,6 @@ func NewListIdentitiesOK() *ListIdentitiesOK {
 /*ListIdentitiesOK handles this case with default header values.
 
 A list of identities.
-
-nolint:deadcode,unused
 */
 type ListIdentitiesOK struct {
 	Payload []*models.Identity
