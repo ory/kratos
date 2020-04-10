77d5f77f8868aa97b1a9e810e26554d5c3c0a1ed
aeneasr <aeneas@ory.sh>
2020-04-10 22:43:57 +0200

    chore: remove node_modules

--- node_modules/@cypress/listr-verbose-renderer/lib/utils.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const chalk = require('chalk');
-const format = require('date-fns/format');
-
-exports.log = (options, output) => {
-	const timestamp = format(new Date(), options.dateFormat);
-
-	console.log(chalk.dim(`[${timestamp}]`) + ` ${output}`);
-};
