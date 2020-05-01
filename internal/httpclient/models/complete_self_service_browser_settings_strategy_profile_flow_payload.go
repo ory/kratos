e3677478eda4f5fe7e3157609edcf690ce1ed04b
aeneasr <aeneas@ory.sh>
2020-05-01 15:24:01 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/models/complete_self_service_browser_settings_strategy_profile_flow_payload.go
+++ internal/httpclient/models/complete_self_service_browser_settings_strategy_profile_flow_payload.go
@@ -7,12 +7,13 @@ package models
 
 import (
 	"github.com/go-openapi/errors"
-	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 	"github.com/go-openapi/validate"
 )
 
 // CompleteSelfServiceBrowserSettingsStrategyProfileFlowPayload complete self service browser settings strategy profile flow payload
+//
 // swagger:model completeSelfServiceBrowserSettingsStrategyProfileFlowPayload
 type CompleteSelfServiceBrowserSettingsStrategyProfileFlowPayload struct {
 
--- internal/httpclient/models/settings_request_method.go
+++ internal/httpclient/models/settings_request_method.go
@@ -7,11 +7,12 @@ package models
 
 import (
 	"github.com/go-openapi/errors"
-	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
 
 // SettingsRequestMethod settings request method
+//
 // swagger:model settingsRequestMethod
 type SettingsRequestMethod struct {
 
