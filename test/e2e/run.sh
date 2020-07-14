c7a4ceded51cfe3a2c3230f720e9ca989d452043
Andreas Bucksteeg <andreas@bucksteeg.de>
2020-07-14 17:08:56 +0200

    fix: unset env var

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -18,7 +18,6 @@ if [ -z ${TEST_DATABASE_POSTGRESQL+x} ]; then
   export TEST_DATABASE_MYSQL="mysql://root:secret@(127.0.0.1:3444)/mysql?parseTime=true&multiStatements=true"
   export TEST_DATABASE_POSTGRESQL="postgres://postgres:secret@127.0.0.1:3445/postgres?sslmode=disable"
   export TEST_DATABASE_COCKROACHDB="cockroach://root@127.0.0.1:3446/defaultdb?sslmode=disable"
-  export TEST_DATABASE_MEMORY="memory"
 
 fi
 
@@ -183,6 +182,7 @@ if [[ $dev = "yes" ]]; then
 fi
 
 export TEST_DATABASE_SQLITE="sqlite:///$(mktemp -d -t ci-XXXXXXXXXX)/db.sqlite?_fk=true"
+export TEST_DATABASE_MEMORY="memory"
 
 case "$1" in
         sqlite)
