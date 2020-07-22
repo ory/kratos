e3aa81b7da55108f43ea6e16c817c97e2f8a1d50
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-07-22 17:47:27 +0200

    feat: add pagination tests

--- x/pagination.go
+++ x/pagination.go
@@ -8,7 +8,8 @@ import (
 	"strings"
 )
 
-const paginationMaxItems = 500
+const paginationMaxItems = 1000
+const paginationDefaultItems = 250
 
 // ParsePagination parses limit and page from *http.Request with given limits and defaults.
 func ParsePagination(r *http.Request) (page, itemsPerPage int) {
@@ -23,10 +24,10 @@ func ParsePagination(r *http.Request) (page, itemsPerPage int) {
 	}
 
 	if limitParam := r.URL.Query().Get("per_page"); limitParam == "" {
-		itemsPerPage = paginationMaxItems
+		itemsPerPage = paginationDefaultItems
 	} else {
 		if limit64, err := strconv.ParseInt(limitParam, 10, 64); err != nil {
-			itemsPerPage = paginationMaxItems
+			itemsPerPage = paginationDefaultItems
 		} else {
 			itemsPerPage = int(limit64)
 		}
@@ -50,7 +51,7 @@ func ParsePagination(r *http.Request) (page, itemsPerPage int) {
 func header(u *url.URL, rel string, limit, page int64) string {
 	q := u.Query()
 	q.Set("per_page", fmt.Sprintf("%d", limit))
-	q.Set("page", fmt.Sprintf("%d", page))
+	q.Set("page", fmt.Sprintf("%d", page/limit))
 	u.RawQuery = q.Encode()
 	return fmt.Sprintf("<%s>; rel=\"%s\"", u.String(), rel)
 }
@@ -109,5 +110,4 @@ func PaginationHeader(w http.ResponseWriter, u *url.URL, total int64, page, item
 		header(u, "prev", itemsPerPage64, ((offset/itemsPerPage64)-1)*itemsPerPage64),
 		header(u, "last", itemsPerPage64, lastOffset),
 	}, ","))
-	return
 }
