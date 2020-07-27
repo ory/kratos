39a9ea139add017006f76204a86e4d3a69d1b772
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-07-27 12:09:51 +0200

    feat: reply with cache-control: 0 for browser-facing APIs
    Closes #360

--- x/router.go
+++ x/router.go
@@ -21,3 +21,33 @@ func NewRouterAdmin() *RouterAdmin {
 		Router: httprouter.New(),
 	}
 }
+
+// GET is a shortcut for router.Handle("GET", path, handle)
+func (r *RouterPublic) GET(path string, handle httprouter.Handle) {
+	r.Handle("GET", path, NoCacheHandler(handle))
+}
+
+// HEAD is a shortcut for router.Handle("HEAD", path, handle)
+func (r *RouterPublic) HEAD(path string, handle httprouter.Handle) {
+	r.Handle("HEAD", path, NoCacheHandler(handle))
+}
+
+// POST is a shortcut for router.Handle("POST", path, handle)
+func (r *RouterPublic) POST(path string, handle httprouter.Handle) {
+	r.Handle("POST", path, NoCacheHandler(handle))
+}
+
+// PUT is a shortcut for router.Handle("PUT", path, handle)
+func (r *RouterPublic) PUT(path string, handle httprouter.Handle) {
+	r.Handle("PUT", path, NoCacheHandler(handle))
+}
+
+// PATCH is a shortcut for router.Handle("PATCH", path, handle)
+func (r *RouterPublic) PATCH(path string, handle httprouter.Handle) {
+	r.Handle("PATCH", path, NoCacheHandler(handle))
+}
+
+// DELETE is a shortcut for router.Handle("DELETE", path, handle)
+func (r *RouterPublic) DELETE(path string, handle httprouter.Handle) {
+	r.Handle("DELETE", path, NoCacheHandler(handle))
+}
