7f1b2c61f326b893f91c15d26c0038bc2391c415
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-08-13 12:51:38 +0200

    feat: support and document api flow in session issuer hook

--- selfservice/hook/session_issuer.go
+++ selfservice/hook/session_issuer.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/ory/kratos/selfservice/flow/login"
+	"github.com/pkg/errors"
+
+	"github.com/ory/kratos/selfservice/flow"
 	"github.com/ory/kratos/selfservice/flow/registration"
 	"github.com/ory/kratos/session"
+	"github.com/ory/kratos/x"
 )
 
 var (
-	_ login.PostHookExecutor                   = new(SessionIssuer)
 	_ registration.PostHookPostPersistExecutor = new(SessionIssuer)
 )
 
@@ -18,6 +20,7 @@ type (
 	sessionIssuerDependencies interface {
 		session.ManagementProvider
 		session.PersistenceProvider
+		x.WriterProvider
 	}
 	SessionIssuerProvider interface {
 		HookSessionIssuer() *SessionIssuer
@@ -36,13 +39,14 @@ func (e *SessionIssuer) ExecutePostRegistrationPostPersistHook(w http.ResponseWr
 	if err := e.r.SessionPersister().CreateSession(r.Context(), s); err != nil {
 		return err
 	}
-	return e.r.SessionManager().IssueCookie(r.Context(), w, r, s)
-}
 
-func (e *SessionIssuer) ExecuteLoginPostHook(w http.ResponseWriter, r *http.Request, a *login.Flow, s *session.Session) error {
-	s.AuthenticatedAt = time.Now().UTC()
-	if err := e.r.SessionPersister().CreateSession(r.Context(), s); err != nil {
-		return err
+	if a.Type == flow.TypeAPI {
+		e.r.Writer().Write(w, r, &registration.APIFlowResponse{
+			Session: s, Token: s.Token,
+			Identity: s.Identity,
+		})
+		return errors.WithStack(registration.ErrHookAbortFlow)
 	}
+
 	return e.r.SessionManager().IssueCookie(r.Context(), w, r, s)
 }
--- selfservice/hook/session_issuer_test.go
+++ selfservice/hook/session_issuer_test.go
@@ -2,6 +2,7 @@ package hook_test
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,12 +10,15 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
 
 	"github.com/ory/viper"
 
 	"github.com/ory/kratos/driver/configuration"
 	"github.com/ory/kratos/identity"
 	"github.com/ory/kratos/internal"
+	"github.com/ory/kratos/selfservice/flow"
+	"github.com/ory/kratos/selfservice/flow/registration"
 	"github.com/ory/kratos/selfservice/hook"
 	"github.com/ory/kratos/session"
 	"github.com/ory/kratos/x"
@@ -28,31 +32,44 @@ func TestSessionIssuer(t *testing.T) {
 	var r http.Request
 	h := hook.NewSessionIssuer(reg)
 
-	t.Run("method=sign-in", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		sid := x.NewUUID()
-
-		i := identity.NewIdentity(configuration.DefaultIdentityTraitsSchemaID)
-		require.NoError(t, reg.PrivilegedIdentityPool().CreateIdentity(context.Background(), i))
-		require.NoError(t, h.ExecuteLoginPostHook(w, &r, nil, &session.Session{ID: sid, Identity: i}))
-
-		got, err := reg.SessionPersister().GetSession(context.Background(), sid)
-		require.NoError(t, err)
-		assert.Equal(t, sid, got.ID)
-		assert.True(t, got.AuthenticatedAt.After(time.Now().Add(-time.Minute)))
-	})
-
 	t.Run("method=sign-up", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		sid := x.NewUUID()
+		t.Run("flow=browser", func(t *testing.T) {
+			w := httptest.NewRecorder()
+			sid := x.NewUUID()
 
-		i := identity.NewIdentity(configuration.DefaultIdentityTraitsSchemaID)
-		require.NoError(t, reg.PrivilegedIdentityPool().CreateIdentity(context.Background(), i))
-		require.NoError(t, h.ExecutePostRegistrationPostPersistHook(w, &r, nil, &session.Session{ID: sid, Identity: i}))
+			i := identity.NewIdentity(configuration.DefaultIdentityTraitsSchemaID)
+			require.NoError(t, reg.PrivilegedIdentityPool().CreateIdentity(context.Background(), i))
+			require.NoError(t, h.ExecutePostRegistrationPostPersistHook(w, &r,
+				&registration.Flow{Type: flow.TypeBrowser}, &session.Session{ID: sid, Identity: i}))
 
-		got, err := reg.SessionPersister().GetSession(context.Background(), sid)
-		require.NoError(t, err)
-		assert.Equal(t, sid, got.ID)
-		assert.True(t, got.AuthenticatedAt.After(time.Now().Add(-time.Minute)))
+			got, err := reg.SessionPersister().GetSession(context.Background(), sid)
+			require.NoError(t, err)
+			assert.Equal(t, sid, got.ID)
+			assert.True(t, got.AuthenticatedAt.After(time.Now().Add(-time.Minute)))
+
+			assert.Contains(t, w.Header().Get("Set-Cookie"), session.DefaultSessionCookieName)
+		})
+
+		t.Run("flow=api", func(t *testing.T) {
+			w := httptest.NewRecorder()
+
+			i := identity.NewIdentity(configuration.DefaultIdentityTraitsSchemaID)
+			s := &session.Session{ID: x.NewUUID(), Identity: i}
+			f := &registration.Flow{Type: flow.TypeAPI}
+
+			require.NoError(t, reg.PrivilegedIdentityPool().CreateIdentity(context.Background(), i))
+			require.True(t, errors.Is(h.ExecutePostRegistrationPostPersistHook(w, &r, f, s), registration.ErrHookAbortFlow))
+
+			got, err := reg.SessionPersister().GetSession(context.Background(), s.ID)
+			require.NoError(t, err)
+			assert.Equal(t, s.ID, got.ID)
+			assert.True(t, got.AuthenticatedAt.After(time.Now().Add(-time.Minute)))
+
+			assert.Empty(t, w.Header().Get("Set-Cookie"))
+			body := w.Body.Bytes()
+			assert.Equal(t, i.ID, gjson.GetBytes(body, "identity.id").String())
+			assert.Equal(t, s.ID, gjson.GetBytes(body, "session.id").String())
+			assert.Equal(t, got.Token, gjson.GetBytes(body, "session_token").String())
+		})
 	})
 }
