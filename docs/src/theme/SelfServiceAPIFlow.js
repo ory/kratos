5a4435643ae3463df85458f22f87730c11af10ab
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-08-24 15:49:11 +0200

    docs: password login and registration methods for API clients

--- docs/src/theme/SelfServiceAPIFlow.js
+++ docs/src/theme/SelfServiceAPIFlow.js
@@ -3,6 +3,8 @@ import Mermaid from "./Mermaid";
 
 const chart = ({
   flows = ['login', 'registration', 'settings', '...'],
+  methods = ['password', 'oidc', '...'],
+  interactions = ['"Log in"', '"Sign Up"', '"Update Email"', '...'],
   success = 'Perform flow-specific action (e.g. create user, set session cookie, ...)'
 }) => `
 sequenceDiagram
@@ -13,11 +15,11 @@ sequenceDiagram
   K-->>K: Create and store new ${flows.join(', ')} flow
   K->>B: HTTP 200 OK with flow as application/json payload
   B-->>B: Render form using e.g. Native iOS UI Elements
-  B-->>B: User fills out forms, clicks e.g. "Log in", "Sign Up", "Update Email", ...
-  B->>K: REST POST to e.g. /self-service/login/methods/password
+  B-->>B: User fills out forms, clicks e.g. ${interactions}
+  B->>K: REST POST to e.g. /self-service/<${flows.join('|')}>/methods/<${methods.join('|')}>
   K-->>K: Validates and processes payload
   alt Form payload is valid valid
-    K->>B: Perform flow-specific action (e.g. create user, return session cookie, ...)
+    K->>B: ${success}
   else Login data invalid
     K-->>K: Update and store flow (e.g. add form validation errors)
     K->>B: Respond with e.g. HTTP 400 Bad Request and updated flow as payload
