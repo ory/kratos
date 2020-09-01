e2f34d3f411bac042079d7f5425063ef117fae77
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-09-01 14:27:21 +0200

    fix: resolve migratest failures

--- persistence/sql/migratest/testdata/20200812160551_testdata.sql
+++ persistence/sql/migratest/testdata/20200812160551_testdata.sql
@@ -1,2 +1,2 @@
 INSERT INTO sessions (id, issued_at, expires_at, authenticated_at,created_at,updated_at, token, identity_id, active)
-VALUES ('f38cdebe-e567-42c9-a562-1bd4dee40998','2013-10-07 08:23:19','2013-10-07 08:23:19','2013-10-07 08:23:19','2013-10-07 08:23:19','2013-10-07 08:23:19', '999bbefa-4a10-4bf8-98d0-61654315d975', '5ff66179-c240-4703-b0d8-494592cefff5', true)
+VALUES ('f38cdebe-e567-42c9-a562-1bd4dee40998','2013-10-07 08:23:19','2013-10-07 08:23:19','2013-10-07 08:23:19','2013-10-07 08:23:19','2013-10-07 08:23:19', '1001ba7ddd644cb68478e8947e4jfhb', '5ff66179-c240-4703-b0d8-494592cefff5', true)
