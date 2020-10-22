be4da789b0f3baae2f089e2bf8ee4a0b5b22e550
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-10-22 11:32:12 +0200

    fix: use correct assert package

--- x/clean_url_test.go
+++ x/clean_url_test.go
@@ -7,7 +7,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/bmizerany/assert"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/urfave/negroni"
 )
