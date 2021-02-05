7bf5889bf1813625574a2ff710c39ee02a8dcb0d
vinckr <vincent@ory.sh>
2021-02-05 14:06:48 +0100

    fix: minor fix & chore

--- docs/scripts/gen-faq.js
+++ docs/scripts/gen-faq.js
@@ -1,20 +1,25 @@
 // read.js
-const fs = require('fs');
-const yaml = require('js-yaml');
-const { Remarkable } = require('remarkable');
-
+const fs = require('fs')
+const yaml = require('js-yaml')
+const { Remarkable } = require('remarkable')
 
 try {
-    // Generating FAQ.mdx
+  // Generating FAQ.mdx
 
-    let fayYaml = fs.readFileSync('./docs/faq.yaml', 'utf8');
-    let faq = yaml.safeLoad(fayYaml);
+  let fayYaml = fs.readFileSync('./docs/faq.yaml', 'utf8')
+  let faq = yaml.load(fayYaml)
 
+  const tags = Array.from(
+    new Set(
+      faq
+        .map((el) => {
+          return el.tags
+        })
+        .flat(1)
+    )
+  )
 
-    const tags = Array.from(new Set(faq.map(el => { return el.tags }).flat(1)))
-
-    
-    data=`---
+  data = `---
 id: faq
 title: Faq
 ---
@@ -27,26 +32,40 @@ import {Question, Faq} from '@theme/Faq'
 <br/><br/>
 
 `
-    md = new Remarkable();
-    faq.forEach(el => {
-        react_tags = el.tags.map((tag) => {return tag+"_src-theme-"})
-        data += `<Question tags="question_src-theme- ${react_tags.join(" ")}">\n`
-        data += `${el.tags.map( tag => {return "#"+tag }).join(" ")} <br/>\n` 
-        data += md.render(`**Q**: ${el.q}`) 
-        data += md.render(`**A**: ${el.a}\n`)
-        if (el.context) {
-            data += md.render(`context: ${el.context}\n`)
-        }
-        data += `</Question>\n\n<br/>`
-    });
+  md = new Remarkable()
+  faq.forEach((el) => {
+    react_tags = el.tags.map((tag) => {
+      return tag + '_src-theme-'
+    })
+    data += `<Question tags="question_src-theme- ${react_tags.join(' ')}">\n`
+    data += `${el.tags
+      .map((tag) => {
+        return '#' + tag
+      })
+      .join(' ')} <br/>\n`
+    data += md.render(`**Q**: ${el.q}`)
+    data += md.render(`**A**: ${el.a}\n`)
+    if (el.context) {
+      data += md.render(`context: ${el.context}\n`)
+    }
+    data += `</Question>\n\n<br/>`
+  })
 
-    fs.writeFile('./docs/docs/faq.mdx', data, (err) => { 
-        if (err) throw err; 
-    }) 
+  fs.writeFile('./docs/docs/faq.mdx', data, (err) => {
+    if (err) throw err
+  })
 
-    // Generating faq.module.css
-    const taglist = Array.from(new Set(faq.map(el => { return el.tags }).flat(1)))
-    css_file=`
+  // Generating faq.module.css
+  const taglist = Array.from(
+    new Set(
+      faq
+        .map((el) => {
+          return el.tags
+        })
+        .flat(1)
+    )
+  )
+  css_file = `
 .pills,
 .tabs {
     font-weight:var(--ifm-font-weight-bold)
@@ -128,8 +147,8 @@ div.question {
     display: none;
 }
 `
-    taglist.forEach( tag => {
-        css_file += `
+  taglist.forEach((tag) => {
+    css_file += `
 li.selected.${tag} {
     color:red;
 }
@@ -139,12 +158,10 @@ li.selected.${tag}~.question.${tag} {
     
 }
 `
-    })
-    fs.writeFile('./docs/src/theme/faq.module.css', css_file, (err) => { 
-        if (err) throw err; 
-    }) 
-
+  })
+  fs.writeFile('./docs/src/theme/faq.module.css', css_file, (err) => {
+    if (err) throw err
+  })
 } catch (e) {
-    throw e
+  throw e
 }
-    
--- docs/src/theme/Faq.js
+++ docs/src/theme/Faq.js
@@ -3,64 +3,56 @@ import cn from 'classnames'
 import styles from './faq.module.css'
 //import questions from './faq_questions.json'
 
+const Question = ({ children, tags }) => <div className={tags}>{children}</div>
 
-
-
-const Question = ({children, tags}) => (
-  <div className={tags}>
+const TagButton = ({ tag, isSelected, children, toggleSelected }) => (
+  <li
+    className={cn(
+      { [styles.selected]: isSelected },
+      tag + '_src-theme-',
+      'pills',
+      'pills__item',
+      { 'styles.pills__item--active': isSelected }
+    )}
+    onClick={toggleSelected}
+  >
     {children}
-  </div>
+  </li>
 )
 
-
-const TagButton = ({ tag,  isSelected, children, toggleSelected  }) => (
-    <li
-          className={cn({ [styles.selected]: isSelected}, tag+"_src-theme-", "pills", "pills__item", {"styles.pills__item--active" : isSelected})}
-          onClick={toggleSelected}
-        >
-          {children}
-        </li>
-)
-
-const Faq = ({tags}) => {
-  tags=tags.split(" ")
+const Faq = ({ tags }) => {
+  tags = tags.split(' ')
   const [selectedTags, setSelectedTags] = useState(tags)
-  
-  
-  const displayFunc = (tags) => { 
+
+  const displayFunc = (tags) => {
     for (var i = 0; i < tags.length; i++) {
-        if (selectedTags.find((t) => t === tags[i]) ) {
-            return 'block'
-        }
+      if (selectedTags.find((t) => t === tags[i])) {
+        return 'block'
+      }
     }
     return 'none'
-  } 
+  }
 
   return (
     <>
-
-      {
-      tags.map((tag) => (
+      {tags.map((tag) => (
         <TagButton
-        key={tag}
-        tag={tag}
-        isSelected={selectedTags.find((t) => t === tag)}
-        toggleSelected={() => {
+          key={tag}
+          tag={tag}
+          isSelected={selectedTags.find((t) => t === tag)}
+          toggleSelected={() => {
             if (selectedTags.find((t) => t === tag)) {
-                setSelectedTags(selectedTags.filter((t) => t !== tag))
+              setSelectedTags(selectedTags.filter((t) => t !== tag))
             } else {
-                setSelectedTags([...selectedTags, tag])
+              setSelectedTags([...selectedTags, tag])
             }
-        }}
+          }}
         >
-            #{tag}
+          #{tag}
         </TagButton>
-      
       ))}
-
-
     </>
   )
 }
 
-export {Faq,Question}
+export { Faq, Question }
