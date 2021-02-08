08a2c1f51eb5dc83aba42f9ab8e8bc38da1fe53e
zepatrik <zepatrik@users.noreply.github.com>
2021-02-08 17:18:45 +0100

    docs: minor improvements - fix: emptyResponse description wrapping to new line - feat: rename "Revoke and Invalidate a Session" to reflect the most common use-case (analogous to other endpoints)

--- selfservice/strategy/handler.go
+++ selfservice/strategy/handler.go
@@ -1,11 +1,13 @@
 package strategy
 
 import (
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
+
 	"github.com/ory/herodot"
 	"github.com/ory/kratos/driver/config"
 	"github.com/ory/kratos/x"
-	"net/http"
 )
 
 const EndpointDisabledMessage = "This endpoint was disabled by system administrator. Please check your url or contact the system administrator to enable it."
