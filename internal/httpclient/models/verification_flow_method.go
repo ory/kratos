16d79f09d5929ba28ebdabe6366189c4cb167c88
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-02-12 16:11:02 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/models/verification_flow_method.go
+++ internal/httpclient/models/verification_flow_method.go
@@ -14,7 +14,7 @@ import (
 	"github.com/go-openapi/validate"
 )
 
-// VerificationFlowMethod verification flow method
+// VerificationFlowMethod VerificationFlowMethod verification flow method
 //
 // swagger:model verificationFlowMethod
 type VerificationFlowMethod struct {
