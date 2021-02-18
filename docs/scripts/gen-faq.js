484a64be4a0afa8ecffa4852e5d76d4c6476abe2
Kim Neunert <kneunert@gmail.com>
2021-02-18 11:52:38 +0100

    suggestions from Patrik

--- docs/scripts/gen-faq.js
+++ docs/scripts/gen-faq.js
@@ -4,6 +4,8 @@ const yaml = require('js-yaml')
 const { Remarkable } = require('remarkable')
 const path = require('path')
 const yamlPath = path.resolve('./docs/faq.yaml')
+const prettier = require('prettier')
+const prettierStyles = require('ory-prettier-styles')
 
 // Generating FAQ.mdx
 
@@ -36,18 +38,18 @@ import {Question, Faq} from '@theme/Faq'
     faq.forEach(el => {
         react_tags = el.tags.map((tag) => {return tag+"_src-theme-"})
         data += `<Question tags="question_src-theme- ${react_tags.join(" ")}">\n`
-        data += `${el.tags.map( tag => {return "#"+tag }).join(" ")} <br/>\n` 
-        data += md.render(`**Q**: ${el.q}`) 
-        data += md.render(`**A**: ${el.a}\n`)
+        data += `    ${el.tags.map( tag => {return "#"+tag }).join(" ")} <br/>\n` 
+        data += "    " + md.render(`**Q**: ${el.q}`) 
+        data += "    " + md.render(`**A**: ${el.a}\n`)
         if (el.context) {
-            data += md.render(`context: ${el.context}\n`)
+            data += "    " + md.render(`context: ${el.context}\n`)
         }
         data += `</Question>\n\n<br/>`
+        
     });
-
-    fs.writeFile('./docs/docs/faq.mdx', data, (err) => { 
-        if (err) throw err; 
-    }) 
+    // Unfortunatly this is a mix of html/markdown and prettier is either not 
+    // properly formatting html or mixing up the syntax (with the html parser)
+    fs.writeFileSync(path.resolve('./docs/docs/faq.mdx'), data)
 
     // Generating faq.module.css
     const taglist = Array.from(new Set(faq.map(el => { return el.tags }).flat(1)))
