c31785ba056eb4d3682b7529822b2ffa99d5535e
Matt Bonnell <matt.bonnell@icloud.com>
2021-03-08 14:13:10 -0500

    update test, add millisecond epsilon to account for rounding errors

--- x/time_test.go
+++ x/time_test.go
@@ -8,11 +8,12 @@ import (
 )
 
 func TestRandomDelay(t *testing.T) {
-	base := time.Millisecond * 2
-	deviation := time.Millisecond
+	base := time.Millisecond * 10
+	deviation := time.Millisecond * 5
+	epsilon := time.Millisecond
 	for i := 0; i < 100; i++ {
 		delay := RandomDelay(base, deviation)
-		require.LessOrEqual(t, delay, base+deviation)
-		require.GreaterOrEqual(t, delay, base-deviation)
+		require.LessOrEqual(t, delay, base+deviation+epsilon)
+		require.GreaterOrEqual(t, delay, base-deviation-epsilon)
 	}
 }
