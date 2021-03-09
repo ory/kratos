b75495a0eb4ae223e10435a98046d4b424ad0cef
Matt Bonnell <matt.bonnell@icloud.com>
2021-03-09 10:08:19 -0500

    add docstring to describe formula

--- x/time.go
+++ x/time.go
@@ -19,6 +19,15 @@ func RequireEqualTime(t *testing.T, expected, actual time.Time) {
 	require.EqualValues(t, expected.UTC().Round(time.Second), actual.UTC().Round(time.Second))
 }
 
+// RandomDelay returns a time randomly chosen from a normal distribution with mean of base and max/min of base +- deviation
+// From the docstring for the rand.NormFloat64():
+// To produce a different normal distribution, callers can
+// adjust the output using:
+//
+//  sample = NormFloat64() * desiredStdDev + desiredMean
+//
+// Since 99.73% of values in a normal distribution lie within three standard deviations from the mean (https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule),
+// by taking the standard deviation to be deviation/3, we can get a distribution which meets our requirements.
 func RandomDelay(base, deviation time.Duration) time.Duration {
 	return time.Duration(rnd.NormFloat64()*(float64(deviation)/3) + float64(base))
 }
