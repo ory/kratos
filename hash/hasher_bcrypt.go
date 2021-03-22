530498b1e22f974a40d26fea25a17282c5bceb96
Sergey Eremenko <seremenko@wish.com>
2021-03-22 15:59:38 -0700

    feature:bcrypt-hasher

---
+++ hash/hasher_bcrypt.go
@@ -0,0 +1,57 @@
+package hash
+
+import (
+	"context"
+	"github.com/ory/kratos/driver/config"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Bcrypt struct {
+	c BcryptConfiguration
+}
+
+type BcryptConfiguration interface {
+	config.Provider
+}
+
+func NewHasherBcrypt(c BcryptConfiguration) *Bcrypt {
+	return &Bcrypt{c: c}
+}
+
+func (h *Bcrypt) Generate(ctx context.Context, password []byte) ([]byte, error) {
+	if err := validatePasswordLength(password); err != nil {
+		return nil, err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword(password, int(h.c.Config(ctx).HasherBcrypt().Cost))
+	if err != nil {
+		return nil, err
+	}
+
+	return hash, nil
+}
+
+func (h *Bcrypt) Compare(_ context.Context, password []byte, hash []byte) error {
+	if err := validatePasswordLength(password); err != nil {
+		return err
+	}
+
+	err := bcrypt.CompareHashAndPassword(hash, password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validatePasswordLength(password []byte) error {
+	// Bcrypt truncates the password to the first 72 bytes, following the OpenBSD implementation,
+	// so if password is longer than 72 bytes, function returns an error
+	// See https://en.wikipedia.org/wiki/Bcrypt#User_input
+	if len(password) > 72 {
+		return errors.New("password is too long")
+	}
+	return nil
+}
+
--- hash/hasher_test.go
+++ hash/hasher_test.go
@@ -20,7 +20,7 @@ func mkpw(t *testing.T, length int) []byte {
 	return pw
 }
 
-func TestHasher(t *testing.T) {
+func TestArgonHasher(t *testing.T) {
 	for k, pw := range [][]byte{
 		mkpw(t, 8),
 		mkpw(t, 16),
@@ -47,6 +47,95 @@ func TestHasher(t *testing.T) {
 					require.Error(t, h.Compare(context.Background(), mod, hs))
 				})
 			}
+		})
+	}
+}
+
+func TestBcryptHasherGeneratesErrorWhenPasswordIsLong(t *testing.T) {
+	_, reg := internal.NewFastRegistryWithMocks(t)
+	hasher := hash.NewHasherBcrypt(reg)
+
+	password := mkpw(t, 73)
+	res, err := hasher.Generate(context.Background(), password)
+
+	assert.Error(t, err, "password is too long")
+	assert.Nil(t, res)
+}
+
+func TestBcryptHasherGeneratesHash(t *testing.T) {
+	for k, pw := range [][]byte{
+		mkpw(t, 8),
+		mkpw(t, 16),
+		mkpw(t, 32),
+		mkpw(t, 64),
+		mkpw(t, 72),
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			_, reg := internal.NewFastRegistryWithMocks(t)
+			hasher := hash.NewHasherBcrypt(reg)
+			res, err := hasher.Generate(context.Background(), pw)
+
+			assert.Nil(t, err)
+
+			// Valid format: $2a$12$[22 character salt][31 character hash]
+			assert.Equal(t, 60, len(string(res)), "invalid bcrypt hash length")
+			assert.Equal(t, "$2a$12$", string(res)[:7], "invalid bcrypt identifier")
+		})
+	}
+}
+
+
+func TestBcryptHasherComparesFailsWhenPasswordIsTooLong(t *testing.T) {
+	_, reg := internal.NewFastRegistryWithMocks(t)
+	hasher := hash.NewHasherBcrypt(reg)
+
+	password := mkpw(t, 73)
+	err := hasher.Compare(context.Background(), password, []byte("hash"))
+
+	assert.Error(t, err, "password is too long")
+}
+
+
+func TestBcryptHasherComparesHashSuccess(t *testing.T) {
+	for k, pw := range [][]byte{
+		mkpw(t, 8),
+		mkpw(t, 16),
+		mkpw(t, 32),
+		mkpw(t, 64),
+		mkpw(t, 72),
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			_, reg := internal.NewFastRegistryWithMocks(t)
+			hasher := hash.NewHasherBcrypt(reg)
+
+			hs, err := hasher.Generate(context.Background(), pw)
+
+			assert.Nil(t, err)
+
+			err = hasher.Compare(context.Background(), pw, hs)
+			assert.Nil(t, err, "hash validation fails")
+		})
+	}
+}
+
+func TestBcryptHasherComparesHashFail(t *testing.T) {
+	for k, pw := range [][]byte{
+		mkpw(t, 8),
+		mkpw(t, 16),
+		mkpw(t, 32),
+		mkpw(t, 64),
+		mkpw(t, 72),
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			_, reg := internal.NewFastRegistryWithMocks(t)
+			hasher := hash.NewHasherBcrypt(reg)
+
+			mod := make([]byte, len(pw))
+			copy(mod, pw)
+			mod[len(pw)-1] = ^pw[len(pw)-1]
+
+			err := hasher.Compare(context.Background(), pw, mod)
+			assert.Error(t, err)
 		})
 	}
 }
