a6cc4294bb46fe3551b8974a7bf959a74587c709
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-04-09 19:48:21 +0200

    ci: resolve test issues

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -152,7 +152,11 @@ run() {
   if [[ $dev = "yes" ]]; then
     npm run test:watch -- --config integrationFolder="test/e2e/cypress/integration/profiles/$profile"
   else
-    npm run test -- --config integrationFolder="test/e2e/cypress/integration/profiles/$profile"
+    if [ -z ${CYPRESS_RECORD_KEY+x} ]; then
+      npm run test -- --config integrationFolder="test/e2e/cypress/integration/profiles/$profile"
+    else
+      npm run test -- --record --config integrationFolder="test/e2e/cypress/integration/profiles/$profile"
+    fi
   fi
 }
 
