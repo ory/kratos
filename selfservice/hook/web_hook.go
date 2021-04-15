ea0a97e149af558d2590c216e41f0178ad005b91
Dimitrij Drus <dimitrij.drus@innoq.com>
2021-04-15 14:58:18 +0200

    Code clean up and preparation for next steps

--- selfservice/hook/web_hook.go
+++ selfservice/hook/web_hook.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ory/kratos/selfservice/flow/verification"
 	"github.com/ory/kratos/session"
 	"github.com/ory/kratos/x"
+	"io"
 	"net/http"
 )
 
@@ -20,8 +21,9 @@ type (
 	}
 
 	webHookConfig struct {
-		method string
-		url    string
+		Method string
+		Url    string
+		Body   string
 	}
 
 	WebHook struct {
@@ -34,32 +36,56 @@ func NewWebHook(r webHookDependencies, c json.RawMessage) *WebHook {
 	return &WebHook{r: r, c: c}
 }
 
-func (e *WebHook) ExecutePostVerificationHook(_ http.ResponseWriter, _ *http.Request, _ *verification.Flow, s *session.Session) error {
-	return e.executeWebHook(s)
+func (e *WebHook) ExecutePostVerificationHook(_ http.ResponseWriter, _ *http.Request, f *verification.Flow, s *session.Session) error {
+	return e.executeWebHook(f, s)
 }
 
-func (e *WebHook) ExecutePostRegistrationPostPersistHook(_ http.ResponseWriter, _ *http.Request, _ *registration.Flow, s *session.Session) error {
-	return e.executeWebHook(s)
+func (e *WebHook) ExecutePostRegistrationPostPersistHook(_ http.ResponseWriter, _ *http.Request, f *registration.Flow, s *session.Session) error {
+	return e.executeWebHook(f, s)
 }
 
-func (e *WebHook) executeWebHook(s *session.Session) error {
+func (e *WebHook) executeWebHook(f interface{}, s *session.Session) error {
 	var conf webHookConfig
 	if err := json.Unmarshal(e.c, &conf); err != nil {
 		e.r.Logger().WithError(err).Error("Error while unmarshalling web_hook config json")
 		return err
 	}
 
-	payloadBuf := new(bytes.Buffer)
-	if err := json.NewEncoder(payloadBuf).Encode(s.Identity); err != nil {
-		e.r.Logger().WithError(err).Error("Error while marshalling web_hook payload json")
+	if body, err := e.createBody(conf, f, s); err != nil {
+		return err
+	} else if err = e.doHttpCall(conf, body); err != nil {
 		return err
 	}
 
-	if response, err := http.Post(conf.url, "application/json", payloadBuf); err != nil {
+	return nil
+}
+
+func (e *WebHook) createBody(_ webHookConfig, _ interface{}, s *session.Session) (io.Reader, error) {
+	// TODO: make use of JSONNet
+	payloadBuf := new(bytes.Buffer)
+	if err := json.NewEncoder(payloadBuf).Encode(s.Identity); err != nil {
+		e.r.Logger().WithError(err).Error("Error while marshalling web_hook payload json")
+		return nil, err
+	}
+
+	return payloadBuf, nil
+}
+
+func (e *WebHook) doHttpCall(conf webHookConfig, body io.Reader) error {
+	req, err := http.NewRequest(conf.Method, conf.Url, body)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// TODO: Make use of authentication/authorization
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
 		e.r.Logger().WithError(err).Warn("Web hook failed")
 		return err
-	} else if response.StatusCode >= 400 {
-		return fmt.Errorf("web hook failed with status code %v", response.StatusCode)
+	} else if resp.StatusCode >= 400 {
+		return fmt.Errorf("web hook failed with status code %v", resp.StatusCode)
 	}
 
 	return nil
