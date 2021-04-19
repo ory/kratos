bdd79f17b9ff3996c11142197558f38e4de17c80
Dimitrij Drus <dimitrij.drus@innoq.com>
2021-04-19 08:24:05 +0200

    code cleanup

--- selfservice/hook/web_hook.go
+++ selfservice/hook/web_hook.go
@@ -19,38 +19,32 @@ var _ registration.PostHookPostPersistExecutor = new(WebHook)
 var _ verification.PostHookExecutor = new(WebHook)
 
 type (
-	webHookDependencies interface {
-		x.LoggingProvider
-	}
-
-	emptyAuthConfig struct{}
-
-	basicAuthConfig struct {
-		User     string
-		Password string
-	}
-
-	apiKeyConfig struct {
-		Name  string
-		Value string
-		In    string
-	}
-
-	AuthConfig interface {
+	AuthStrategy interface {
 		apply(req *http.Request)
 	}
 
-	Auth struct {
-		Type       string
-		AuthConfig AuthConfig      `json:"-"`
-		RawConfig  json.RawMessage `json:"config"`
+	noopAuthStrategy struct{}
+
+	basicAuthStrategy struct {
+		user     string
+		password string
+	}
+
+	apiKeyStrategy struct {
+		name  string
+		value string
+		in    string
 	}
 
 	webHookConfig struct {
-		Method string
-		Url    string
-		Body   string
-		Auth   Auth
+		method       string
+		url          string
+		templatePath string
+		auth         AuthStrategy
+	}
+
+	webHookDependencies interface {
+		x.LoggingProvider
 	}
 
 	WebHook struct {
@@ -59,57 +53,96 @@ type (
 	}
 )
 
-func (c *emptyAuthConfig) apply(_ *http.Request) {}
-
-func (c *webHookConfig) Unmarshal(bytes []byte) error {
-	if err := json.Unmarshal(bytes, c); err != nil {
-		return err
+func newWebHookConfig(r json.RawMessage) (*webHookConfig, error) {
+	type rawWebHookConfig struct {
+		Method string
+		Url    string
+		Body   string
+		Auth   struct {
+			Type   string
+			Config json.RawMessage
+		}
 	}
 
-	if len(c.Auth.Type) == 0 {
-		c.Auth.AuthConfig = &emptyAuthConfig{}
-	}
-	return nil
-}
-
-func (c *basicAuthConfig) apply(req *http.Request) {
-	req.SetBasicAuth(c.User, c.Password)
-}
-
-func (c *apiKeyConfig) apply(req *http.Request) {
-	switch c.In {
-	case "cookie":
-		req.AddCookie(&http.Cookie{Name: c.Name, Value: c.Value})
-	default:
-		req.Header.Set(c.Name, c.Value)
-	}
-}
-
-func (a *Auth) UnmarshalJSON(bytes []byte) error {
-	type auth Auth
-	err := json.Unmarshal(bytes, (*auth)(a))
+	var rc rawWebHookConfig
+	err := json.Unmarshal(r, &rc)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	switch a.Type {
-	case "basic-auth":
-		var authConfig basicAuthConfig
-		err = json.Unmarshal(a.RawConfig, &authConfig)
-		if err != nil {
-			return err
-		}
-		a.AuthConfig = &authConfig
+
+	var as AuthStrategy
+	switch rc.Auth.Type {
+	case "":
+		as = &noopAuthStrategy{}
 	case "api-key":
-		var authConfig apiKeyConfig
-		err = json.Unmarshal(a.RawConfig, &authConfig)
-		if err != nil {
-			return err
-		}
-		a.AuthConfig = &authConfig
+		as, err = newApiKeyStrategy(rc.Auth.Config)
+	case "basic-auth":
+		as, err = newBasicAuthStrategy(rc.Auth.Config)
 	default:
-		return fmt.Errorf("unknown auth type %v", a.Type)
+		err = fmt.Errorf("unsupported auth type: %s", rc.Auth.Type)
 	}
-	return nil
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create web hook auth strategy: %w", err)
+	}
+
+	return &webHookConfig{
+		method:       rc.Method,
+		url:          rc.Url,
+		templatePath: rc.Body,
+		auth:         as,
+	}, nil
+}
+
+func (c *noopAuthStrategy) apply(_ *http.Request) {}
+
+func newBasicAuthStrategy(raw json.RawMessage) (*basicAuthStrategy, error) {
+	type config struct {
+		User     string
+		Password string
+	}
+
+	var c config
+	if err := json.Unmarshal(raw, &c); err != nil {
+		return nil, err
+	}
+
+	return &basicAuthStrategy{
+		user:     c.User,
+		password: c.Password,
+	}, nil
+}
+
+func (c *basicAuthStrategy) apply(req *http.Request) {
+	req.SetBasicAuth(c.user, c.password)
+}
+
+func newApiKeyStrategy(raw json.RawMessage) (*apiKeyStrategy, error) {
+	type config struct {
+		In    string
+		Name  string
+		Value string
+	}
+
+	var c config
+	if err := json.Unmarshal(raw, &c); err != nil {
+		return nil, err
+	}
+
+	return &apiKeyStrategy{
+		in:    c.In,
+		name:  c.Name,
+		value: c.Value,
+	}, nil
+}
+
+func (c *apiKeyStrategy) apply(req *http.Request) {
+	switch c.in {
+	case "cookie":
+		req.AddCookie(&http.Cookie{Name: c.name, Value: c.value})
+	default:
+		req.Header.Set(c.name, c.value)
+	}
 }
 
 func NewWebHook(r webHookDependencies, c json.RawMessage) *WebHook {
@@ -117,32 +150,36 @@ func NewWebHook(r webHookDependencies, c json.RawMessage) *WebHook {
 }
 
 func (e *WebHook) ExecutePostVerificationHook(_ http.ResponseWriter, _ *http.Request, f *verification.Flow, s *session.Session) error {
-	return e.executeWebHook(f, s)
+	return e.execute(f, s)
 }
 
 func (e *WebHook) ExecutePostRegistrationPostPersistHook(_ http.ResponseWriter, _ *http.Request, f *registration.Flow, s *session.Session) error {
-	return e.executeWebHook(f, s)
+	return e.execute(f, s)
 }
 
-func (e *WebHook) executeWebHook(f interface{}, s interface{}) (err error) {
-	conf := &webHookConfig{}
-	if err := conf.Unmarshal(e.c); err != nil {
+func (e *WebHook) execute(f interface{}, s interface{}) error {
+	conf, err := newWebHookConfig(e.c)
+	if err != nil {
 		return err
 	}
-	var body io.Reader
-	if len(conf.Body) != 0 {
-		body, err = e.createBody(conf.Body, f, s)
-		if err != nil {
-			return fmt.Errorf("failed to create web hook body: %w", err)
-		}
+
+	body, err := createBody(conf.templatePath, f, s)
+	if err != nil {
+		return fmt.Errorf("failed to create web hook body: %w", err)
 	}
-	if err = e.doHttpCall(conf, body); err != nil {
+
+	if err = doHttpCall(conf.method, conf.url, conf.auth, body); err != nil {
 		return fmt.Errorf("failed to call web hook %w", err)
 	}
 	return nil
 }
 
-func (e *WebHook) createBody(jsonnetFile string, f interface{}, s interface{}) (io.Reader, error) {
+func createBody(templatePath string, f, s interface{}) (io.Reader, error) {
+	var body io.Reader
+	if len(templatePath) == 0 {
+		return body, nil
+	}
+
 	vm := jsonnet.MakeVM()
 
 	buf := new(bytes.Buffer)
@@ -161,21 +198,21 @@ func (e *WebHook) createBody(jsonnetFile string, f interface{}, s interface{}) (
 	}
 	vm.TLACode("session", buf.String())
 
-	if res, err := vm.EvaluateFile(jsonnetFile); err != nil {
+	if res, err := vm.EvaluateFile(templatePath); err != nil {
 		return nil, err
 	} else {
 		return bytes.NewReader([]byte(res)), nil
 	}
 }
 
-func (e *WebHook) doHttpCall(conf *webHookConfig, body io.Reader) error {
-	req, err := http.NewRequest(conf.Method, conf.Url, body)
+func doHttpCall(method string, url string, as AuthStrategy, body io.Reader) error {
+	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	conf.Auth.AuthConfig.apply(req)
+	as.apply(req)
 
 	resp, err := http.DefaultClient.Do(req)
 
