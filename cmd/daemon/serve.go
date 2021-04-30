87484d63bb76724fa7af413ec061bd8f103cfbe5
Sergey Eremenko <seremenko@wish.com>
2021-04-29 18:18:37 -0700

    fix: Prometheus URL label

--- cmd/daemon/serve.go
+++ cmd/daemon/serve.go
@@ -89,6 +89,7 @@ func ServePublic(r driver.Registry, wg *sync.WaitGroup, cmd *cobra.Command, args
 	n.UseHandler(r.CSRFHandler())
 
 	r.RegisterPublicRoutes(router)
+	r.PrometheusManager().RegisterRouter(router)
 	n.Use(reqlog.NewMiddlewareFromLogger(l, "public#"+c.SelfPublicURL().String()))
 	n.Use(sqa(cmd, r))
 	n.Use(r.PrometheusManager())
@@ -128,6 +129,7 @@ func ServeAdmin(r driver.Registry, wg *sync.WaitGroup, cmd *cobra.Command, args 
 
 	router := x.NewRouterAdmin()
 	r.RegisterAdminRoutes(router)
+	r.PrometheusManager().RegisterRouter(router)
 	n.Use(reqlog.NewMiddlewareFromLogger(l, "admin#"+c.SelfPublicURL().String()))
 	n.Use(sqa(cmd, r))
 	n.Use(r.PrometheusManager())
--- metrics/prometheus/middleware.go
+++ metrics/prometheus/middleware.go
@@ -1,12 +1,15 @@
 package prometheus
 
 import (
+	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"strings"
 	"time"
 )
 
 type MetricsManager struct {
 	prometheusMetrics *Metrics
+	routers           []*httprouter.Router
 }
 
 func NewMetricsManager(version, hash, buildTime string) *MetricsManager {
@@ -20,5 +23,45 @@ func (pmm *MetricsManager) ServeHTTP(rw http.ResponseWriter, r *http.Request, ne
 	start := time.Now()
 	next(rw, r)
 
-	pmm.prometheusMetrics.ResponseTime.WithLabelValues(r.URL.Path).Observe(time.Since(start).Seconds())
+	pmm.prometheusMetrics.ResponseTime.WithLabelValues(
+		pmm.getLabelForPath(r),
+	).Observe(time.Since(start).Seconds())
+}
+
+func (pmm *MetricsManager) RegisterRouter(router *httprouter.Router) {
+	pmm.routers = append(pmm.routers, router)
+}
+
+func (pmm *MetricsManager) getLabelForPath(r *http.Request) string {
+	// looking for a match in one of registered routers
+	for _, router := range pmm.routers {
+		handler, params, _ := router.Lookup(r.Method, r.URL.Path)
+		if handler != nil {
+			return reconstructEndpoint(r.URL.Path, params)
+		}
+	}
+	return "{unmatched}"
+}
+
+// To reduce cardinality of labels, values of matched path parameters must be replaced with {param}
+func reconstructEndpoint(path string, params httprouter.Params) string {
+	// if map is empty, then nothing to change in the path
+	if len(params) == 0 {
+		return path
+	}
+
+	// construct a list of parameter values
+	paramValues := make(map[string]struct{}, len(params))
+	for _, param := range params {
+		paramValues[param.Value] = struct{}{}
+	}
+
+	parts := strings.Split(path, "/")
+	for index, part := range parts {
+		if _, ok := paramValues[part]; ok {
+			parts[index] = "{param}"
+		}
+	}
+
+	return strings.Join(parts, "/")
 }
