ad8aaf15b497a4561433afc81bd4bcd1b9d304ee
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-05-17 13:12:15 +0200

    fix: properly handle CSRF for API flows in recovery and verification strategies
    Closes #1141

--- selfservice/flow/recovery/handler.go
+++ selfservice/flow/recovery/handler.go
@@ -59,6 +59,7 @@ func NewHandler(d handlerDependencies) *Handler {
 }
 
 func (h *Handler) RegisterPublicRoutes(public *x.RouterPublic) {
+	h.d.CSRFHandler().IgnorePath(RouteSubmitFlow)
 	h.d.CSRFHandler().IgnorePath(RouteInitAPIFlow)
 
 	redirect := session.RedirectOnAuthenticated(h.d)
