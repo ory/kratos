c9a07a4b16d71c7c84e12f6cb25fb20cbd60d04f
Dimitrij Drus <dimitrij.drus@innoq.com>
2021-05-18 10:44:09 +0200

    the actual implementation of the require_verified_address hook

---
+++ selfservice/hook/address_verifier.go
@@ -0,0 +1,30 @@
+package hook
+
+import (
+	"net/http"
+
+	"github.com/ory/kratos/selfservice/flow/login"
+	"github.com/ory/kratos/session"
+	"github.com/pkg/errors"
+)
+
+var _ login.PostHookExecutor = new(SessionDestroyer)
+
+var ErrAddressNotVerified   = errors.New("address not verified yet")
+
+type AddressVerifier struct {}
+
+
+func NewAddressVerifier() *AddressVerifier {
+	return &AddressVerifier{}
+}
+
+func (e *AddressVerifier) ExecuteLoginPostHook(_ http.ResponseWriter, _ *http.Request, _ *login.Flow, s *session.Session) error {
+	// all addresses, the user is using for identification purposes, must be verified
+	for _, va := range s.Identity.VerifiableAddresses {
+		if !va.Verified {
+			return ErrAddressNotVerified
+		}
+	}
+	return nil
+}
---
+++ selfservice/hook/address_verifier_test.go
@@ -0,0 +1,108 @@
+package hook
+
+import (
+	"testing"
+
+	"github.com/gofrs/uuid"
+	"github.com/ory/kratos/identity"
+	"github.com/ory/kratos/session"
+	"github.com/ory/kratos/x"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddressVerifier(t *testing.T) {
+	verifier := NewAddressVerifier()
+
+	t.Run("Single Address Not Verified", func(t *testing.T) {
+		s := &session.Session{ID: x.NewUUID(), Identity: &identity.Identity{
+			ID: x.NewUUID(),
+			VerifiableAddresses: []identity.VerifiableAddress{
+				{
+					ID:       uuid.UUID{},
+					Verified: false,
+				},
+			},
+		}}
+
+		err := verifier.ExecuteLoginPostHook(nil, nil, nil, s)
+
+		assert.ErrorIs(t, err, ErrAddressNotVerified)
+	})
+
+	t.Run("Single Address Verified", func(t *testing.T) {
+		s := &session.Session{ID: x.NewUUID(), Identity: &identity.Identity{
+			ID: x.NewUUID(),
+			VerifiableAddresses: []identity.VerifiableAddress{
+				{
+					ID:       uuid.UUID{},
+					Verified: true,
+				},
+			},
+		}}
+
+		err := verifier.ExecuteLoginPostHook(nil, nil, nil, s)
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("Multiple Addresses Verified", func(t *testing.T) {
+		s := &session.Session{ID: x.NewUUID(), Identity: &identity.Identity{
+			ID: x.NewUUID(),
+			VerifiableAddresses: []identity.VerifiableAddress{
+				{
+					ID:       uuid.UUID{},
+					Verified: true,
+				},
+				{
+					ID:       uuid.UUID{},
+					Verified: true,
+				},
+			},
+		}}
+
+		err := verifier.ExecuteLoginPostHook(nil, nil, nil, s)
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("Multiple Addresses Not Verified", func(t *testing.T) {
+		s := &session.Session{ID: x.NewUUID(), Identity: &identity.Identity{
+			ID: x.NewUUID(),
+			VerifiableAddresses: []identity.VerifiableAddress{
+				{
+					ID:       uuid.UUID{},
+					Verified: false,
+				},
+				{
+					ID:       uuid.UUID{},
+					Verified: false,
+				},
+			},
+		}}
+
+		err := verifier.ExecuteLoginPostHook(nil, nil, nil, s)
+
+		assert.ErrorIs(t, err, ErrAddressNotVerified)
+	})
+
+	t.Run("One Address Verified And One Not", func(t *testing.T) {
+		s := &session.Session{ID: x.NewUUID(), Identity: &identity.Identity{
+			ID: x.NewUUID(),
+			VerifiableAddresses: []identity.VerifiableAddress{
+				{
+					ID:       uuid.UUID{},
+					Verified: true,
+				},
+				{
+					ID:       uuid.UUID{},
+					Verified: false,
+				},
+			},
+		}}
+
+		err := verifier.ExecuteLoginPostHook(nil, nil, nil, s)
+
+		assert.ErrorIs(t, err, ErrAddressNotVerified)
+	})
+
+}
