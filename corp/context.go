fbdcb91e913d53b2284e91a1415d761b899715d0
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-06-08 17:32:28 +0200

    u

--- corp/context.go
+++ corp/context.go
@@ -16,30 +16,30 @@ type Contextualizer interface {
 	ContextualizeNID(ctx context.Context, fallback uuid.UUID) uuid.UUID
 }
 
-var Context Contextualizer = nil
+var c Contextualizer = nil
 
-func SetContextualizer(c Contextualizer) {
-	if _, ok := c.(*ContextNoOp); ok && Context != nil {
+func SetContextualizer(cc Contextualizer) {
+	if _, ok := cc.(*ContextNoOp); ok && c != nil {
 		return
 	}
 
-	Context = c
+	c = cc
 }
 
 // These global functions call the respective method on Context
 
 func ContextualizeTableName(ctx context.Context, name string) string {
-	return Context.ContextualizeTableName(ctx, name)
+	return c.ContextualizeTableName(ctx, name)
 }
 
 func ContextualizeMiddleware(ctx context.Context) func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	return Context.ContextualizeMiddleware(ctx)
+	return c.ContextualizeMiddleware(ctx)
 }
 
 func ContextualizeConfig(ctx context.Context, fb *config.Config) *config.Config {
-	return Context.ContextualizeConfig(ctx, fb)
+	return c.ContextualizeConfig(ctx, fb)
 }
 
 func ContextualizeNID(ctx context.Context, fallback uuid.UUID) uuid.UUID {
-	return Context.ContextualizeNID(ctx, fallback)
+	return c.ContextualizeNID(ctx, fallback)
 }
