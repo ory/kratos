af7d31d3d80b8916475af29b4d5c1d7244fce780
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-06-08 17:36:03 +0200

    u

---
+++ corp/context_test.go
@@ -0,0 +1,31 @@
+package corp
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+type contextMock struct{
+	ContextNoOp
+}
+
+func (*contextMock) ContextualizeTableName(_ context.Context, name string) string {
+	return "foo"
+}
+
+func TestSetContextualizer(t *testing.T) {
+	ctx := context.Background()
+	require.Panics(t, func() {
+		ContextualizeTableName(ctx, "")
+	})
+
+	SetContextualizer(new(ContextNoOp))
+	assert.Equal(t, "bar", ContextualizeTableName(ctx, "bar"))
+
+	SetContextualizer(new(contextMock))
+	assert.Equal(t, "foo", ContextualizeTableName(ctx, "bar"))
+
+	SetContextualizer(new(ContextNoOp))
+	assert.Equal(t, "foo", ContextualizeTableName(ctx, "bar"))
+}
