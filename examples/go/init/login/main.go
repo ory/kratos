e948faddce3a1f52df964c701f6ba2a28f5dfe03
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-06-15 22:19:56 +0200

    docs: add go sdk examples

---
+++ examples/go/init/login/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	ory "github.com/ory/kratos-client-go"
+	"log"
+)
+
+func sdk(endpoint string) *ory.APIClient {
+	conf := ory.NewConfiguration()
+	conf.Servers = ory.ServerConfigurations{{URL: endpoint}}
+	return ory.NewAPIClient(conf)
+}
+
+func initLogin() *ory.LoginFlow {
+	ctx := context.Background()
+
+	// If you use Open Source this would be:
+	//
+	// c := sdk("https://127.0.0.1:4433")
+	c := sdk("https://playground.projects.oryapis.com/api/kratos/public")
+
+	flow, _, err := c.PublicApi.InitializeSelfServiceLoginWithoutBrowser(ctx).Execute()
+	if err != nil {
+		log.Fatalf("An error ocurred: %s\n", err)
+	}
+
+	return flow
+}
+
+func main() {
+	flow := initLogin()
+	out, _ := json.MarshalIndent(flow, "", "  ")
+	fmt.Println(string(out))
+}
