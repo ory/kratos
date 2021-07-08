f5e96cd5054e734c319ed32992357fcd73ac44a1
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-07-08 17:58:32 +0200

    docs: refactor documentation and adopt changes for #1477
    Closes #1472

--- docs/src/theme/Code/CodeTabs.js
+++ docs/src/theme/Code/CodeTabs.js
@@ -2,28 +2,49 @@ import Tabs from '@theme/Tabs'
 import TabItem from '@theme/TabItem'
 import React from 'react'
 import CodeBlock from '@theme/CodeBlock'
+import CodeFromRemote from '@theme/CodeFromRemote'
 
-const GetFlow = ({ items }) => (
-  <Tabs
-    defaultValue={Object.keys(items)[0]}
-    values={Object.keys(items).map((key) => ({
-      label: items[key].label,
-      value: key
-    }))}
-  >
-    {Object.keys(items).map((key) => (
-      <TabItem key={key} value={key}>
-        {items[key].code ? (
-          <CodeBlock
-            className={`language-${items[key].language}`}
-            children={items[key].code}
-          />
-        ) : (
-          <img src={items[key].image} alt={items[key].alt} />
-        )}
-      </TabItem>
-    ))}
-  </Tabs>
-)
+const FlowContent = ({ item }) => {
+  if (item.code) {
+    console.warn(item.code, 'asfd')
+    return (
+      <CodeBlock className={`language-${item.language}`} children={item.code} />
+    )
+  }
+
+  if (item.image) {
+    return <img src={item.image} alt={item.alt} />
+  }
+
+  if (item.codeFromRemote) {
+    return (
+      <CodeFromRemote
+        language={item.language}
+        link={item.codeFromRemote.link}
+        src={item.codeFromRemote.src}
+      />
+    )
+  }
+
+  return <span>Unknown item type: ${JSON.stringify(item)}</span>
+}
+
+const GetFlow = ({ items }) => {
+  return (
+    <Tabs
+      defaultValue={Object.keys(items)[0]}
+      values={Object.keys(items).map((key) => ({
+        label: items[key].label,
+        value: key
+      }))}
+    >
+      {Object.keys(items).map((key) => (
+        <TabItem key={key} value={key}>
+          <FlowContent item={items[key]} />
+        </TabItem>
+      ))}
+    </Tabs>
+  )
+}
 
 export default GetFlow
