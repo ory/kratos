3cfd7845730685a4493c2b5d1974b79d873eea86
ttimonen <toni.timonen@iki.fi>
2021-07-15 09:04:00 +0200

    fix: cleanup lint errors and add doc to x (#1545)

---
+++ x/doc.go
@@ -0,0 +1,8 @@
+/*
+Package x provides various helpers that do not have an obvious home elsewhere.
+
+The contract implied here, is that:
+ - Package x does not depend on other parts of kratos
+ - Packages outside kratos do not depend on x.
+*/
+package x
