5c9a49487f45af5b7edf069edf9c3d37ef293cd5
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-13 11:24:46 +0200

    feat: sort totp nodes

--- selfservice/flow/settings/sort.go
+++ selfservice/flow/settings/sort.go
@@ -1,6 +1,8 @@
 package settings
 
-import "github.com/ory/kratos/ui/node"
+import (
+	"github.com/ory/kratos/ui/node"
+)
 
 func sortNodes(n node.Nodes, schemaRef string) error {
 	return n.SortBySchema(
@@ -9,7 +11,15 @@ func sortNodes(n node.Nodes, schemaRef string) error {
 			node.DefaultGroup,
 			node.ProfileGroup,
 			node.PasswordGroup,
+			node.TOTPGroup,
 			node.OpenIDConnectGroup,
+		}),
+		node.SortUseOrder([]string{
+			// TOTP
+			node.TOTPSecretKey,
+			node.TOTPQR,
+			node.TOTPUnlink,
+			node.TOTPCode,
 		}),
 	)
 }
