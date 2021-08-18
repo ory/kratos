d20e65defac82f55406f86eeaa81abd0635d646c
emnicolas <73804452+emnicolas@users.noreply.github.com>
2021-08-18 16:17:59 +0900

    [+] identify UBnity version in startup logs

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright © UBnity Co.,Ltd. All Rights Reserved.
 
 // package main is the entry point for kratos.
 package main
 
 import (
+	"fmt"
 	"github.com/ory/kratos/driver"
 	"github.com/ory/x/dbal"
 	"github.com/ory/x/profilex"
@@ -24,6 +13,8 @@ import (
 )
 
 func main() {
+	fmt.Println("Running UBnity version of Kratos")
+
 	defer profilex.Profile().Stop()
 	dbal.RegisterDriver(func() dbal.Driver {
 		return driver.NewRegistryDefault()
