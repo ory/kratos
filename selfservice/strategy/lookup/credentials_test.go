0f818c3aa21f28bd6fe24cbe2f4823c88d337f12
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-23 14:44:56 +0200

    test: resolve flaky tests

--- selfservice/strategy/lookup/credentials_test.go
+++ selfservice/strategy/lookup/credentials_test.go
@@ -15,10 +15,10 @@ var fixtureNode []byte
 
 func TestToNode(t *testing.T) {
 	c := CredentialsConfig{RecoveryCodes: []RecoveryCode{
-		{Code: "foo", UsedAt: sqlxx.NullTime(time.Unix(1629199958, 0))},
+		{Code: "foo", UsedAt: sqlxx.NullTime(time.Unix(1629199958, 0).UTC())},
 		{Code: "bar"},
 		{Code: "baz"},
-		{Code: "oof", UsedAt: sqlxx.NullTime(time.Unix(1629199968, 0))},
+		{Code: "oof", UsedAt: sqlxx.NullTime(time.Unix(1629199968, 0).UTC())},
 	}}
 
 	assertx.EqualAsJSON(t, json.RawMessage(fixtureNode), c.ToNode())
