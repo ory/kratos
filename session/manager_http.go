5a160f653bb99a06271673e2c8e6609a0fe6f865
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-30 17:31:45 +0300

    fix: do not panic if cookiemanager returns a nil cookie
    Closes #1695

--- session/manager_http.go
+++ session/manager_http.go
@@ -52,7 +52,11 @@ func (s *ManagerHTTP) CreateAndIssueCookie(ctx context.Context, w http.ResponseW
 }
 
 func (s *ManagerHTTP) IssueCookie(ctx context.Context, w http.ResponseWriter, r *http.Request, session *Session) error {
-	cookie, _ := s.r.CookieManager(r.Context()).Get(r, s.cookieName(ctx))
+	cookie, err := s.r.CookieManager(r.Context()).Get(r, s.cookieName(ctx))
+	// Fix for https://github.com/ory/kratos/issues/1695
+	if err != nil && cookie == nil {
+		return errors.WithStack(err)
+	}
 
 	if s.r.Config(ctx).SessionPath() != "" {
 		cookie.Options.Path = s.r.Config(ctx).SessionPath()
