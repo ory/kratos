8feff8daaf4ac744fab22627d9bdab45740570d5
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-31 12:05:00 +0300

    feat: redirect on login if session aal is not matched

--- selfservice/flow/login/hook.go
+++ selfservice/flow/login/hook.go
@@ -60,6 +60,12 @@ func NewHookExecutor(d executorDependencies) *HookExecutor {
 	return &HookExecutor{d: d}
 }
 
+func (e *HookExecutor) requiresAAL2(r *http.Request, s *session.Session) (*session.ErrAALNotSatisfied, bool) {
+	var aalErr *session.ErrAALNotSatisfied
+	err := e.d.SessionManager().DoesSessionSatisfy(r, s, e.d.Config(r.Context()).SessionWhoAmIAAL())
+	return aalErr, errors.As(err, &aalErr)
+}
+
 func (e *HookExecutor) PostLoginHook(w http.ResponseWriter, r *http.Request, a *Flow, i *identity.Identity, s *session.Session) error {
 	if err := s.Activate(i, e.d.Config(r.Context()), time.Now().UTC()); err != nil {
 		return err
@@ -107,7 +113,13 @@ func (e *HookExecutor) PostLoginHook(w http.ResponseWriter, r *http.Request, a *
 			WithField("identity_id", i.ID).
 			Info("Identity authenticated successfully and was issued an Ory Kratos Session Token.")
 
-		e.d.Writer().Write(w, r, &APIFlowResponse{Session: s, Token: s.Token})
+		response := &APIFlowResponse{Session: s, Token: s.Token}
+		if _, required := e.requiresAAL2(r, s); required {
+			// If AAL is not satisfied, we omit the identity to preserve the user's privacy in case of a phishing attack.
+			response.Session.Identity = nil
+		}
+
+		e.d.Writer().Write(w, r, response)
 		return nil
 	}
 
@@ -124,7 +136,19 @@ func (e *HookExecutor) PostLoginHook(w http.ResponseWriter, r *http.Request, a *
 	if x.IsJSONRequest(r) {
 		// Browser flows rely on cookies. Adding tokens in the mix will confuse consumers.
 		s.Token = ""
-		e.d.Writer().Write(w, r, &APIFlowResponse{Session: s})
+
+		response := &APIFlowResponse{Session: s}
+		if _, required := e.requiresAAL2(r, s); required {
+			// If AAL is not satisfied, we omit the identity to preserve the user's privacy in case of a phishing attack.
+			response.Session.Identity = nil
+		}
+		e.d.Writer().Write(w, r, response)
+		return nil
+	}
+
+	// If we detect that whoami would require a higher AAL, we redirect!
+	if aalErr, required := e.requiresAAL2(r, s); required {
+		http.Redirect(w, r, aalErr.RedirectTo, http.StatusSeeOther)
 		return nil
 	}
 
--- selfservice/flow/login/hook_test.go
+++ selfservice/flow/login/hook_test.go
@@ -1,10 +1,13 @@
 package login_test
 
 import (
+	"context"
 	"net/http"
 	"net/url"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/ory/kratos/session"
 
@@ -32,7 +35,7 @@ func TestLoginExecutor(t *testing.T) {
 			conf.MustSet(config.ViperKeyDefaultIdentitySchemaURL, "file://./stub/login.schema.json")
 			conf.MustSet(config.ViperKeySelfServiceBrowserDefaultReturnTo, "https://www.ory.sh/")
 
-			newServer := func(t *testing.T, ft flow.Type) *httptest.Server {
+			newServer := func(t *testing.T, ft flow.Type, useIdentity *identity.Identity) *httptest.Server {
 				router := httprouter.New()
 
 				router.GET("/login/pre", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -47,8 +50,12 @@ func TestLoginExecutor(t *testing.T) {
 					a.RequestURL = x.RequestURL(r).String()
 					sess := session.NewInactiveSession()
 					sess.CompletedLoginFor(identity.CredentialsTypePassword)
+					if useIdentity == nil {
+						useIdentity = testhelpers.SelfServiceHookCreateFakeIdentity(t, reg)
+					}
+
 					testhelpers.SelfServiceHookLoginErrorHandler(t, w, r,
-						reg.LoginHookExecutor().PostLoginHook(w, r, a, testhelpers.SelfServiceHookCreateFakeIdentity(t, reg), sess))
+						reg.LoginHookExecutor().PostLoginHook(w, r, a, useIdentity, sess))
 				})
 
 				ts := httptest.NewServer(router)
@@ -64,7 +71,7 @@ func TestLoginExecutor(t *testing.T) {
 				t.Run("case=pass without hooks", func(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 
-					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{})
+					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
 					assert.EqualValues(t, "https://www.ory.sh/", res.Request.URL.String())
 				})
@@ -73,7 +80,7 @@ func TestLoginExecutor(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 					viperSetPost(t, conf, strategy, []config.SelfServiceHook{{Name: "err", Config: []byte(`{}`)}})
 
-					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{})
+					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
 					assert.EqualValues(t, "https://www.ory.sh/", res.Request.URL.String())
 				})
@@ -82,7 +89,7 @@ func TestLoginExecutor(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 					viperSetPost(t, conf, strategy, []config.SelfServiceHook{{Name: "err", Config: []byte(`{"ExecuteLoginPostHook": "abort"}`)}})
 
-					res, body := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{})
+					res, body := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
 					assert.Equal(t, "", body)
 				})
@@ -90,7 +97,7 @@ func TestLoginExecutor(t *testing.T) {
 				t.Run("case=prevent return_to value because domain not whitelisted", func(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 
-					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{"return_to": {"https://www.ory.sh/kratos/"}})
+					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{"return_to": {"https://www.ory.sh/kratos/"}})
 					assert.EqualValues(t, http.StatusInternalServerError, res.StatusCode)
 				})
 
@@ -98,7 +105,7 @@ func TestLoginExecutor(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 					conf.MustSet(config.ViperKeyURLsWhitelistedReturnToDomains, []string{"https://www.ory.sh/"})
 
-					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{"return_to": {"https://www.ory.sh/kratos/"}})
+					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{"return_to": {"https://www.ory.sh/kratos/"}})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
 					assert.EqualValues(t, "https://www.ory.sh/kratos/", res.Request.URL.String())
 				})
@@ -107,7 +114,7 @@ func TestLoginExecutor(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 					conf.MustSet(config.ViperKeySelfServiceLoginAfter+"."+config.DefaultBrowserReturnURL, "https://www.ory.sh/kratos")
 
-					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{})
+					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
 					assert.EqualValues(t, "https://www.ory.sh/kratos/", res.Request.URL.String())
 				})
@@ -117,7 +124,7 @@ func TestLoginExecutor(t *testing.T) {
 					testhelpers.SelfServiceHookLoginSetDefaultRedirectTo(t, conf, "https://www.ory.sh/not-kratos")
 					testhelpers.SelfServiceHookLoginSetDefaultRedirectToStrategy(t, conf, strategy, "https://www.ory.sh/kratos")
 
-					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{})
+					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
 					assert.EqualValues(t, "https://www.ory.sh/kratos/", res.Request.URL.String())
 				})
@@ -126,7 +133,7 @@ func TestLoginExecutor(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 					viperSetPost(t, conf, strategy, []config.SelfServiceHook{{Name: "err", Config: []byte(`{}`)}})
 
-					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser), false, url.Values{})
+					res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), false, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
 					assert.EqualValues(t, "https://www.ory.sh/", res.Request.URL.String())
 				})
@@ -134,19 +141,88 @@ func TestLoginExecutor(t *testing.T) {
 				t.Run("case=send a json response for API clients", func(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 
-					res, body := makeRequestPost(t, newServer(t, flow.TypeAPI), true, url.Values{})
+					res, body := makeRequestPost(t, newServer(t, flow.TypeAPI, nil), true, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
-					assert.NotEmpty(t, gjson.Get(body, "session.identity.id"))
+					assert.NotEmpty(t, gjson.Get(body, "session.identity.id").String())
 				})
 
 				t.Run("case=pass without hooks for browser flow with application/json", func(t *testing.T) {
 					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
 
-					res, body := makeRequestPost(t, newServer(t, flow.TypeBrowser), true, url.Values{})
+					res, body := makeRequestPost(t, newServer(t, flow.TypeBrowser, nil), true, url.Values{})
 					assert.EqualValues(t, http.StatusOK, res.StatusCode)
-					assert.NotEmpty(t, gjson.Get(body, "session.identity.id"))
-					assert.Empty(t, gjson.Get(body, "session.token"))
-					assert.Empty(t, gjson.Get(body, "session_token"))
+					assert.NotEmpty(t, gjson.Get(body, "session.identity.id").String())
+					assert.Empty(t, gjson.Get(body, "session.token").String())
+					assert.Empty(t, gjson.Get(body, "session_token").String())
+				})
+
+				t.Run("case=work normally if AAL is satisfied", func(t *testing.T) {
+					conf.MustSet(config.ViperKeySessionWhoAmIAAL, "aal1")
+					_ = testhelpers.NewLoginUIFlowEchoServer(t, reg)
+					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
+
+					useIdentity := &identity.Identity{Credentials: map[identity.CredentialsType]identity.Credentials{
+						identity.CredentialsTypePassword: {Type: identity.CredentialsTypePassword},
+						identity.CredentialsTypeTOTP:     {Type: identity.CredentialsTypeTOTP},
+					}}
+					require.NoError(t, reg.Persister().CreateIdentity(context.Background(), useIdentity))
+
+					t.Run("browser client", func(t *testing.T) {
+						res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, useIdentity), false, url.Values{})
+						assert.EqualValues(t, http.StatusOK, res.StatusCode)
+						assert.EqualValues(t, "https://www.ory.sh/", res.Request.URL.String())
+					})
+
+					t.Run("api client returns the token but not the identity", func(t *testing.T) {
+						res, body := makeRequestPost(t, newServer(t, flow.TypeAPI, useIdentity), true, url.Values{})
+						assert.EqualValues(t, http.StatusOK, res.StatusCode)
+						assert.NotEmpty(t, gjson.Get(body, "session.identity").String())
+						assert.NotEmpty(t, gjson.Get(body, "session_token").String())
+					})
+
+					t.Run("browser JSON client returns the token but not the identity", func(t *testing.T) {
+						res, body := makeRequestPost(t, newServer(t, flow.TypeBrowser, useIdentity), true, url.Values{})
+						assert.EqualValues(t, http.StatusOK, res.StatusCode)
+						assert.NotEmpty(t, gjson.Get(body, "session.id").String())
+						assert.NotEmpty(t, gjson.Get(body, "session.identity").String())
+						assert.Empty(t, gjson.Get(body, "session_token").String())
+					})
+				})
+
+				t.Run("case=redirect to login if AAL is too low", func(t *testing.T) {
+					conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+					_ = testhelpers.NewLoginUIFlowEchoServer(t, reg)
+					t.Cleanup(func() {
+						conf.MustSet(config.ViperKeySessionWhoAmIAAL, "aal1")
+					})
+					t.Cleanup(testhelpers.SelfServiceHookConfigReset(t, conf))
+
+					useIdentity := &identity.Identity{Credentials: map[identity.CredentialsType]identity.Credentials{
+						identity.CredentialsTypePassword: {Type: identity.CredentialsTypePassword},
+						identity.CredentialsTypeTOTP:     {Type: identity.CredentialsTypeTOTP},
+					}}
+					require.NoError(t, reg.Persister().CreateIdentity(context.Background(), useIdentity))
+
+					t.Run("browser client", func(t *testing.T) {
+						res, _ := makeRequestPost(t, newServer(t, flow.TypeBrowser, useIdentity), false, url.Values{})
+						assert.EqualValues(t, http.StatusNotFound, res.StatusCode)
+						assert.Contains(t, res.Request.URL.String(), "/self-service/login/browser?aal=aal2")
+					})
+
+					t.Run("api client returns the token but not the identity", func(t *testing.T) {
+						res, body := makeRequestPost(t, newServer(t, flow.TypeAPI, useIdentity), true, url.Values{})
+						assert.EqualValues(t, http.StatusOK, res.StatusCode)
+						assert.Empty(t, gjson.Get(body, "session.identity").String())
+						assert.NotEmpty(t, gjson.Get(body, "session_token").String())
+					})
+
+					t.Run("browser JSON client returns the token but not the identity", func(t *testing.T) {
+						res, body := makeRequestPost(t, newServer(t, flow.TypeBrowser, useIdentity), true, url.Values{})
+						assert.EqualValues(t, http.StatusOK, res.StatusCode)
+						assert.NotEmpty(t, gjson.Get(body, "session.id").String())
+						assert.Empty(t, gjson.Get(body, "session.identity").String())
+						assert.Empty(t, gjson.Get(body, "session_token").String())
+					})
 				})
 			})
 
@@ -155,7 +231,7 @@ func TestLoginExecutor(t *testing.T) {
 					config.ViperKeySelfServiceLoginBeforeHooks,
 					testhelpers.SelfServiceMakeLoginPreHookRequest,
 					func(t *testing.T) *httptest.Server {
-						return newServer(t, flow.TypeAPI)
+						return newServer(t, flow.TypeAPI, nil)
 					},
 					conf,
 				))
@@ -166,7 +242,7 @@ func TestLoginExecutor(t *testing.T) {
 					config.ViperKeySelfServiceLoginBeforeHooks,
 					testhelpers.SelfServiceMakeLoginPreHookRequest,
 					func(t *testing.T) *httptest.Server {
-						return newServer(t, flow.TypeBrowser)
+						return newServer(t, flow.TypeBrowser, nil)
 					},
 					conf,
 				))
