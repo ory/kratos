a5c7b1143bca7029bf94fc42fe638534961a06bc
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-31 10:26:21 +0300

    feat: add aal fallback for existing sessions

--- persistence/sql/migrations/sql/20210817181232000001_unique_credentials.cockroach.up.sql
+++ persistence/sql/migrations/sql/20210817181232000001_unique_credentials.cockroach.up.sql
@@ -1,2 +1,1 @@
-ALTER TABLE "identity_credential_identifiers"
-  ADD COLUMN "identity_credential_type_id" UUID;
+ALTER TABLE "identity_credential_identifiers" ADD COLUMN "identity_credential_type_id" UUID;
--- persistence/sql/migrations/sql/20210817181232000008_unique_credentials.sqlite3.up.sql
+++ persistence/sql/migrations/sql/20210817181232000008_unique_credentials.sqlite3.up.sql
@@ -1,4 +1,1 @@
-INSERT INTO "_identity_credential_identifiers_tmp" (id, identifier, identity_credential_id, created_at, updated_at, nid,
-                                                    identity_credential_type_id)
-SELECT id, identifier, identity_credential_id, created_at, updated_at, nid, identity_credential_type_id
-FROM "identity_credential_identifiers";
+INSERT INTO "_identity_credential_identifiers_tmp" (id, identifier, identity_credential_id, created_at, updated_at, nid, identity_credential_type_id) SELECT id, identifier, identity_credential_id, created_at, updated_at, nid, identity_credential_type_id FROM "identity_credential_identifiers";
