70b1932b64d98d24afc4647ea8e988c8f58c195a
mech <wozniak.jacek@gmail.com>
2021-09-24 17:23:54 +0200

    fix: json_bool CR suggestion and tests

--- x/json_bool.go
+++ x/json_bool.go
@@ -9,11 +9,12 @@ type ConvertibleBoolean bool
 
 func (bit *ConvertibleBoolean) UnmarshalJSON(data []byte) error {
 	asString := string(data)
-	if asString == "true" || asString == `"true"` {
+	switch asString {
+	case "true", `"true"`:
 		*bit = true
-	} else if asString == "false" || asString == `"false"` {
+	case "false", `"false"`:
 		*bit = false
-	} else {
+	default:
 		return fmt.Errorf("boolean unmarshal error: invalid input %s", asString)
 	}
 	return nil
