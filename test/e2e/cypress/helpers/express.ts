30423c92ba27709e003e88e58072b78ef3e2aa04
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-01 19:13:29 +0200

    test(e2e): add more routes to registry

--- test/e2e/cypress/helpers/express.ts
+++ test/e2e/cypress/helpers/express.ts
@@ -2,5 +2,7 @@ import {APP_URL} from "./index";
 
 export const routes = {
   base: APP_URL,
-  login: APP_URL + '/login'
+  login: APP_URL + '/login',
+  registration: APP_URL + '/registration',
+  settings: APP_URL + '/settings',
 }
