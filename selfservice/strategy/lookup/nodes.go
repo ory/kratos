efe272f06966edc4858602d94740b6ed36c12e57
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-05 13:54:44 +0200

    feat: use initial_value in lookup strategy

--- selfservice/strategy/lookup/nodes.go
+++ selfservice/strategy/lookup/nodes.go
@@ -6,25 +6,31 @@ import (
 )
 
 func NewRevealLookupNode() *node.Node {
-	return node.NewInputField(node.LookupReveal, "true", node.LookupGroup,
-		node.InputAttributeTypeSubmit).
+	return node.NewInputField(node.LookupReveal, "true", node.LookupGroup, node.InputAttributeTypeSubmit,
+		node.WithInputAttributes(func(a *node.InputAttributes) {
+			a.InitialValue = "false"
+		})).
 		WithMetaLabel(text.NewInfoSelfServiceSettingsRevealLookup())
 }
 
 func NewRegenerateLookupNode() *node.Node {
-	return node.NewInputField(node.LookupRegenerate, "true", node.LookupGroup,
-		node.InputAttributeTypeSubmit).
+	return node.NewInputField(
+		node.LookupRegenerate, "true", node.LookupGroup, node.InputAttributeTypeSubmit, node.WithInputAttributes(func(a *node.InputAttributes) {
+			a.InitialValue = "false"
+		})).
 		WithMetaLabel(text.NewInfoSelfServiceSettingsRegenerateLookup())
 }
 
 func NewDisableLookupNode() *node.Node {
-	return node.NewInputField(node.LookupDisable, "true", node.LookupGroup,
-		node.InputAttributeTypeSubmit).
+	return node.NewInputField(node.LookupDisable, "true", node.LookupGroup, node.InputAttributeTypeSubmit, node.WithInputAttributes(func(a *node.InputAttributes) {
+		a.InitialValue = "false"
+	})).
 		WithMetaLabel(text.NewInfoSelfServiceSettingsDisableLookup())
 }
 
 func NewConfirmLookupNode() *node.Node {
-	return node.NewInputField(node.LookupConfirm, "true", node.LookupGroup,
-		node.InputAttributeTypeSubmit).
+	return node.NewInputField(node.LookupConfirm, "true", node.LookupGroup, node.InputAttributeTypeSubmit, node.WithInputAttributes(func(a *node.InputAttributes) {
+		a.InitialValue = "false"
+	})).
 		WithMetaLabel(text.NewInfoSelfServiceSettingsLookupConfirm())
 }
