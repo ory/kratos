7d8798560947227d64a35d2dd69623bc1a1ddc8f
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-07 17:24:14 +0200

    test(e2e): add config for proxy to simplify cy.visit logic

--- test/e2e/proxy/proxy.js
+++ test/e2e/proxy/proxy.js
@@ -1,6 +1,7 @@
 const request = require('request')
 const urljoin = require('url-join')
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
@@ -14,11 +15,19 @@ const proxy = (base, prefix = null) => (req, res, next) => {
     .pipe(res)
 }
 
-app.use('/self-service/', proxy(process.env.KRATOS_PUBLIC_URL,'/self-service/'))
-app.use('/schemas/', proxy(process.env.KRATOS_PUBLIC_URL,'/schemas/'))
-app.use('/.well-known/', proxy(process.env.KRATOS_PUBLIC_URL,'/.well-known/'))
+app.use('/self-service/', proxy(process.env.KRATOS_PUBLIC_URL, '/self-service/'))
+app.use('/schemas/', proxy(process.env.KRATOS_PUBLIC_URL, '/schemas/'))
+app.use('/.well-known/', proxy(process.env.KRATOS_PUBLIC_URL, '/.well-known/'))
 
-app.use('/', proxy(process.env.KRATOS_UI_URL))
+app.use('/', (req, res, next) => {
+  const pc = fs.readFileSync(require.resolve('../proxy.json'))
+  console.log(pc,JSON.parse(pc.toString()) )
+  if (JSON.parse(pc.toString()) === 'react') {
+    proxy(process.env.KRATOS_UI_REACT_URL)(req, res, next)
+  } else {
+    proxy(process.env.KRATOS_UI_URL)(req, res, next)
+  }
+})
 
 const port = parseInt(process.env.PORT) || 4455
 
