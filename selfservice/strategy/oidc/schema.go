3ef0655e413fc7449ce8aa0b935d52057f97be2c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-18 13:53:12 +0200

    fix: decoder regression in registration

--- selfservice/strategy/oidc/schema.go
+++ selfservice/strategy/oidc/schema.go
@@ -4,8 +4,5 @@ import (
 	_ "embed"
 )
 
-//go:embed .schema/login.schema.json
-var loginSchema []byte
-
-//go:embed .schema/registration.schema.json
-var registrationSchema []byte
+//go:embed .schema/link.schema.json
+var linkSchema []byte
