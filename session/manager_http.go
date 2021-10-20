77ce709d53d88f70c892ab0892c13e16f5b761a5
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-20 22:49:59 +0200

    fix: ensure correct error propagation

--- session/manager_http.go
+++ session/manager_http.go
@@ -196,8 +196,8 @@ func (s *ManagerHTTP) DoesSessionSatisfy(r *http.Request, sess *Session, request
 			return nil
 		}
 
-		return errors.WithStack(NewErrAALNotSatisfied(
-			urlx.CopyWithQuery(urlx.AppendPaths(s.r.Config(r.Context()).SelfPublicURL(r), "/self-service/login/browser"), url.Values{"aal": {"aal2"}}).String()))
+		return NewErrAALNotSatisfied(
+			urlx.CopyWithQuery(urlx.AppendPaths(s.r.Config(r.Context()).SelfPublicURL(r), "/self-service/login/browser"), url.Values{"aal": {"aal2"}}).String())
 	}
 	return errors.Errorf("requested unknown aal: %s", requestedAAL)
 }
