4c5677f3ea48bd87e5d7a1f95e3807b7884a0b64
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-24 00:12:42 +0200

    fix: only respect required modules for SDK

--- internal/httpclient/model_self_service_browser_location_change_required_error.go
+++ internal/httpclient/model_self_service_browser_location_change_required_error.go
@@ -17,17 +17,33 @@ import (
 
 // SelfServiceBrowserLocationChangeRequiredError struct for SelfServiceBrowserLocationChangeRequiredError
 type SelfServiceBrowserLocationChangeRequiredError struct {
-	Error *GenericError `json:"error,omitempty"`
+	// The status code
+	Code *int64 `json:"code,omitempty"`
+	// Debug information  This field is often not exposed to protect against leaking sensitive information.
+	Debug *string `json:"debug,omitempty"`
+	// Further error details
+	Details map[string]map[string]interface{} `json:"details,omitempty"`
+	// The error ID  Useful when trying to identify various errors in application logic.
+	Id *string `json:"id,omitempty"`
+	// Error message  The error's message.
+	Message string `json:"message"`
+	// A human-readable reason for the error
+	Reason *string `json:"reason,omitempty"`
 	// Since when the flow has expired
 	RedirectBrowserTo *string `json:"redirect_browser_to,omitempty"`
+	// The request ID  The request ID is often exposed internally in order to trace errors across service architectures. This is often a UUID.
+	Request *string `json:"request,omitempty"`
+	// The status description
+	Status *string `json:"status,omitempty"`
 }
 
 // NewSelfServiceBrowserLocationChangeRequiredError instantiates a new SelfServiceBrowserLocationChangeRequiredError object
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed
-func NewSelfServiceBrowserLocationChangeRequiredError() *SelfServiceBrowserLocationChangeRequiredError {
+func NewSelfServiceBrowserLocationChangeRequiredError(message string) *SelfServiceBrowserLocationChangeRequiredError {
 	this := SelfServiceBrowserLocationChangeRequiredError{}
+	this.Message = message
 	return &this
 }
 
@@ -39,36 +55,188 @@ func NewSelfServiceBrowserLocationChangeRequiredErrorWithDefaults() *SelfService
 	return &this
 }
 
-// GetError returns the Error field value if set, zero value otherwise.
-func (o *SelfServiceBrowserLocationChangeRequiredError) GetError() GenericError {
-	if o == nil || o.Error == nil {
-		var ret GenericError
+// GetCode returns the Code field value if set, zero value otherwise.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetCode() int64 {
+	if o == nil || o.Code == nil {
+		var ret int64
 		return ret
 	}
-	return *o.Error
+	return *o.Code
 }
 
-// GetErrorOk returns a tuple with the Error field value if set, nil otherwise
+// GetCodeOk returns a tuple with the Code field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *SelfServiceBrowserLocationChangeRequiredError) GetErrorOk() (*GenericError, bool) {
-	if o == nil || o.Error == nil {
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetCodeOk() (*int64, bool) {
+	if o == nil || o.Code == nil {
 		return nil, false
 	}
-	return o.Error, true
+	return o.Code, true
 }
 
-// HasError returns a boolean if a field has been set.
-func (o *SelfServiceBrowserLocationChangeRequiredError) HasError() bool {
-	if o != nil && o.Error != nil {
+// HasCode returns a boolean if a field has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) HasCode() bool {
+	if o != nil && o.Code != nil {
 		return true
 	}
 
 	return false
 }
 
-// SetError gets a reference to the given GenericError and assigns it to the Error field.
-func (o *SelfServiceBrowserLocationChangeRequiredError) SetError(v GenericError) {
-	o.Error = &v
+// SetCode gets a reference to the given int64 and assigns it to the Code field.
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetCode(v int64) {
+	o.Code = &v
+}
+
+// GetDebug returns the Debug field value if set, zero value otherwise.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetDebug() string {
+	if o == nil || o.Debug == nil {
+		var ret string
+		return ret
+	}
+	return *o.Debug
+}
+
+// GetDebugOk returns a tuple with the Debug field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetDebugOk() (*string, bool) {
+	if o == nil || o.Debug == nil {
+		return nil, false
+	}
+	return o.Debug, true
+}
+
+// HasDebug returns a boolean if a field has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) HasDebug() bool {
+	if o != nil && o.Debug != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetDebug gets a reference to the given string and assigns it to the Debug field.
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetDebug(v string) {
+	o.Debug = &v
+}
+
+// GetDetails returns the Details field value if set, zero value otherwise.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetDetails() map[string]map[string]interface{} {
+	if o == nil || o.Details == nil {
+		var ret map[string]map[string]interface{}
+		return ret
+	}
+	return o.Details
+}
+
+// GetDetailsOk returns a tuple with the Details field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetDetailsOk() (map[string]map[string]interface{}, bool) {
+	if o == nil || o.Details == nil {
+		return nil, false
+	}
+	return o.Details, true
+}
+
+// HasDetails returns a boolean if a field has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) HasDetails() bool {
+	if o != nil && o.Details != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetDetails gets a reference to the given map[string]map[string]interface{} and assigns it to the Details field.
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetDetails(v map[string]map[string]interface{}) {
+	o.Details = v
+}
+
+// GetId returns the Id field value if set, zero value otherwise.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetId() string {
+	if o == nil || o.Id == nil {
+		var ret string
+		return ret
+	}
+	return *o.Id
+}
+
+// GetIdOk returns a tuple with the Id field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetIdOk() (*string, bool) {
+	if o == nil || o.Id == nil {
+		return nil, false
+	}
+	return o.Id, true
+}
+
+// HasId returns a boolean if a field has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) HasId() bool {
+	if o != nil && o.Id != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetId gets a reference to the given string and assigns it to the Id field.
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetId(v string) {
+	o.Id = &v
+}
+
+// GetMessage returns the Message field value
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetMessage() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+
+	return o.Message
+}
+
+// GetMessageOk returns a tuple with the Message field value
+// and a boolean to check if the value has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetMessageOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Message, true
+}
+
+// SetMessage sets field value
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetMessage(v string) {
+	o.Message = v
+}
+
+// GetReason returns the Reason field value if set, zero value otherwise.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetReason() string {
+	if o == nil || o.Reason == nil {
+		var ret string
+		return ret
+	}
+	return *o.Reason
+}
+
+// GetReasonOk returns a tuple with the Reason field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetReasonOk() (*string, bool) {
+	if o == nil || o.Reason == nil {
+		return nil, false
+	}
+	return o.Reason, true
+}
+
+// HasReason returns a boolean if a field has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) HasReason() bool {
+	if o != nil && o.Reason != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetReason gets a reference to the given string and assigns it to the Reason field.
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetReason(v string) {
+	o.Reason = &v
 }
 
 // GetRedirectBrowserTo returns the RedirectBrowserTo field value if set, zero value otherwise.
@@ -103,13 +271,98 @@ func (o *SelfServiceBrowserLocationChangeRequiredError) SetRedirectBrowserTo(v s
 	o.RedirectBrowserTo = &v
 }
 
+// GetRequest returns the Request field value if set, zero value otherwise.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetRequest() string {
+	if o == nil || o.Request == nil {
+		var ret string
+		return ret
+	}
+	return *o.Request
+}
+
+// GetRequestOk returns a tuple with the Request field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetRequestOk() (*string, bool) {
+	if o == nil || o.Request == nil {
+		return nil, false
+	}
+	return o.Request, true
+}
+
+// HasRequest returns a boolean if a field has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) HasRequest() bool {
+	if o != nil && o.Request != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetRequest gets a reference to the given string and assigns it to the Request field.
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetRequest(v string) {
+	o.Request = &v
+}
+
+// GetStatus returns the Status field value if set, zero value otherwise.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetStatus() string {
+	if o == nil || o.Status == nil {
+		var ret string
+		return ret
+	}
+	return *o.Status
+}
+
+// GetStatusOk returns a tuple with the Status field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) GetStatusOk() (*string, bool) {
+	if o == nil || o.Status == nil {
+		return nil, false
+	}
+	return o.Status, true
+}
+
+// HasStatus returns a boolean if a field has been set.
+func (o *SelfServiceBrowserLocationChangeRequiredError) HasStatus() bool {
+	if o != nil && o.Status != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetStatus gets a reference to the given string and assigns it to the Status field.
+func (o *SelfServiceBrowserLocationChangeRequiredError) SetStatus(v string) {
+	o.Status = &v
+}
+
 func (o SelfServiceBrowserLocationChangeRequiredError) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
-	if o.Error != nil {
-		toSerialize["error"] = o.Error
+	if o.Code != nil {
+		toSerialize["code"] = o.Code
+	}
+	if o.Debug != nil {
+		toSerialize["debug"] = o.Debug
+	}
+	if o.Details != nil {
+		toSerialize["details"] = o.Details
+	}
+	if o.Id != nil {
+		toSerialize["id"] = o.Id
+	}
+	if true {
+		toSerialize["message"] = o.Message
+	}
+	if o.Reason != nil {
+		toSerialize["reason"] = o.Reason
 	}
 	if o.RedirectBrowserTo != nil {
 		toSerialize["redirect_browser_to"] = o.RedirectBrowserTo
+	}
+	if o.Request != nil {
+		toSerialize["request"] = o.Request
+	}
+	if o.Status != nil {
+		toSerialize["status"] = o.Status
 	}
 	return json.Marshal(toSerialize)
 }
