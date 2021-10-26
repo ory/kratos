d19eede5e6bcffefe3cb64b78addc3d26ed4e2ba
abador <przemyslaw@czaus.pl>
2021-10-26 08:50:41 +0200

    try to fix tests

--- persistence/sql/migrations/sql/20210913095309000003_identity_recovery_tokens.mysql.up.sql
+++ persistence/sql/migrations/sql/20210913095309000003_identity_recovery_tokens.mysql.up.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `identity_recovery_tokens` ADD CONSTRAINT `identity_recovery_tokens_identity_id_fk_idx` FOREIGN KEY (`identity_id`) REFERENCES `identities` (`id`) ON UPDATE RESTRICT ON DELETE CASCADE;
+DELETE FROM identity_recovery_tokens WHERE identity_recovery_address_id IS NULL AND identity_id = '';
