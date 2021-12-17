4180b99afc9427e4ded8398685ee3f0f91e70ddd
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-12-17 11:05:04 +0000

    autogen(docs): generate and format documentation

--- internal/httpclient/model_verifiable_identity_address.go
+++ internal/httpclient/model_verifiable_identity_address.go
@@ -3,7 +3,7 @@
  *
  * Documentation for all public and administrative Ory Kratos APIs. Public and administrative APIs are exposed on different ports. Public APIs can face the public internet without any protection while administrative APIs should never be exposed without prior authorization. To protect the administative API port you should use something like Nginx, Ory Oathkeeper, or any other technology capable of authorizing incoming requests.
  *
- * API version: 1.0.0
+ * API version: v0.8.2-alpha.1
  * Contact: hi@ory.sh
  */
 
