790716e58a4be06f04f3cbc5b974f16d873ae0d8
Alano Terblanche <alanoterblanche@gmail.com>
2021-12-27 18:58:21 +0100

    feat: expose courier template load (#2082)

--- courier/template/recovery_valid.go
+++ courier/template/recovery_valid.go
@@ -25,15 +25,15 @@ func (t *RecoveryValid) EmailRecipient() (string, error) {
 }
 
 func (t *RecoveryValid) EmailSubject() (string, error) {
-	return loadTextTemplate(t.c.CourierTemplatesRoot(), "recovery/valid/email.subject.gotmpl", "recovery/valid/email.subject*", t.m)
+	return LoadTextTemplate(t.c.CourierTemplatesRoot(), "recovery/valid/email.subject.gotmpl", "recovery/valid/email.subject*", t.m)
 }
 
 func (t *RecoveryValid) EmailBody() (string, error) {
-	return loadHTMLTemplate(t.c.CourierTemplatesRoot(), "recovery/valid/email.body.gotmpl", "recovery/valid/email.body*", t.m)
+	return LoadHTMLTemplate(t.c.CourierTemplatesRoot(), "recovery/valid/email.body.gotmpl", "recovery/valid/email.body*", t.m)
 }
 
 func (t *RecoveryValid) EmailBodyPlaintext() (string, error) {
-	return loadTextTemplate(t.c.CourierTemplatesRoot(), "recovery/valid/email.body.plaintext.gotmpl", "recovery/valid/email.body.plaintext*", t.m)
+	return LoadTextTemplate(t.c.CourierTemplatesRoot(), "recovery/valid/email.body.plaintext.gotmpl", "recovery/valid/email.body.plaintext*", t.m)
 }
 
 func (t *RecoveryValid) MarshalJSON() ([]byte, error) {
