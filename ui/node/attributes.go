67ae1877d5e92f609438899a67fce6988d635b86
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-01-02 14:54:57 +0200

    feat: add preset CSP nonce
    Closes https://github.com/ory/kratos-selfservice-ui-node/issues/162

--- ui/node/attributes.go
+++ ui/node/attributes.go
@@ -197,6 +197,15 @@ type ScriptAttributes struct {
 	// required: true
 	Identifier string `json:"id"`
 
+	// Nonce for CSP
+	//
+	// A nonce you may want to use to improve your Content Security Policy.
+	// You do not have to use this value but if you want to improve your CSP
+	// policies you may use it. You can also choose to use your own nonce value!
+	//
+	// required: true
+	Nonce string `json:"nonce"`
+
 	// NodeType represents this node's types. It is a mirror of `node.type` and
 	// is primarily used to allow compatibility with OpenAPI 3.0.
 	//
