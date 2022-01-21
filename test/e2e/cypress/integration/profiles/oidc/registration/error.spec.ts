a60738510875f770f9dbb0b3449dbcf2d473ada3
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-01-21 11:21:16 +0100

    test(e2e): resolve regressions and flakes

--- test/e2e/cypress/integration/profiles/oidc/registration/error.spec.ts
+++ test/e2e/cypress/integration/profiles/oidc/registration/error.spec.ts
@@ -27,7 +27,7 @@ context('Social Sign Up Errors', () => {
       })
 
       it('should fail when the login request is rejected', () => {
-        cy.triggerOidc()
+        cy.triggerOidc(app)
         cy.get('#reject').click()
         cy.location('pathname').should('equal', '/registration')
         cy.get(appPrefix(app) + '[data-testid="ui/message/4000001"]').should(
@@ -39,7 +39,7 @@ context('Social Sign Up Errors', () => {
 
       it('should fail when the consent request is rejected', () => {
         const email = gen.email()
-        cy.triggerOidc()
+        cy.triggerOidc(app)
         cy.get('#username').type(email)
         cy.get('#accept').click()
         cy.get('#reject').click()
@@ -53,7 +53,7 @@ context('Social Sign Up Errors', () => {
 
       it('should fail when the id_token is missing', () => {
         const email = gen.email()
-        cy.triggerOidc()
+        cy.triggerOidc(app)
         cy.get('#username').type(email)
         cy.get('#accept').click()
         cy.get('#website').type(website)
