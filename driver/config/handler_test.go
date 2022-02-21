502b3c0151c85369eb70d8b7c383eb9a976f8664
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-21 17:30:35 +0100

    u

--- driver/config/handler_test.go
+++ driver/config/handler_test.go
@@ -25,13 +25,21 @@ func TestNewConfigHashHandler(t *testing.T) {
 	require.Equal(t, 200, res.StatusCode)
 	first, err := ioutil.ReadAll(res.Body)
 	require.NoError(t, err)
+
+	res, err = ts.Client().Get(ts.URL + "/health/config")
+	require.NoError(t, err)
+	defer res.Body.Close()
+	require.Equal(t, 200, res.StatusCode)
 	second, err := ioutil.ReadAll(res.Body)
 	require.NoError(t, err)
-	assert.NotEqual(t, first, second)
 	assert.Equal(t, first, second)
 
 	require.NoError(t, conf.Set(config.ViperKeySessionDomain, "foobar"))
 
+	res, err = ts.Client().Get(ts.URL + "/health/config")
+	require.NoError(t, err)
+	defer res.Body.Close()
+	require.Equal(t, 200, res.StatusCode)
 	second, err = ioutil.ReadAll(res.Body)
 	require.NoError(t, err)
 	assert.NotEqual(t, first, second)
