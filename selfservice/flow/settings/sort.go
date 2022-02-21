6cca8074a6b25a36d7ef295e285cbea147e42534
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-21 12:12:08 +0100

    fix: appropriately pass context around
    Closes https://github.com/ory/cloud/issues/56

--- selfservice/flow/settings/sort.go
+++ selfservice/flow/settings/sort.go
@@ -2,13 +2,10 @@ package settings
 
 import (
 	"context"
-
 	"github.com/ory/kratos/ui/node"
 )
 
-var ctx = context.Background()
-
-func sortNodes(n node.Nodes, schemaRef string) error {
+func sortNodes(ctx context.Context, n node.Nodes, schemaRef string) error {
 	return n.SortBySchema(ctx,
 		node.SortBySchema(schemaRef),
 		node.SortByGroups([]node.Group{
