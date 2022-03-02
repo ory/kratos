89b26db678b2d11bdc9c4c1e6066f44241007671
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-03-02 17:50:52 +0100

    feat(webauthn): passwordless login

--- selfservice/strategy/webauthn/credentials_test.go
+++ selfservice/strategy/webauthn/credentials_test.go
@@ -1,6 +1,7 @@
 package webauthn
 
 import (
+	"github.com/ory/kratos/identity"
 	"testing"
 
 	"github.com/duo-labs/webauthn/webauthn"
@@ -22,8 +23,17 @@ func TestCredentialConversion(t *testing.T) {
 	actual := CredentialFromWebAuthn(expected, false).ToWebAuthn()
 	assert.Equal(t, expected, actual)
 
-	actualList := Credentials{*CredentialFromWebAuthn(expected, false)}.ToWebAuthn()
+	actualList := Credentials{*CredentialFromWebAuthn(expected, false)}.ToWebAuthn(identity.AuthenticatorAssuranceLevel2)
 	assert.Equal(t, []webauthn.Credential{*expected}, actualList)
+
+	actualList = Credentials{*CredentialFromWebAuthn(expected, true)}.ToWebAuthn(identity.AuthenticatorAssuranceLevel1)
+	assert.Equal(t, []webauthn.Credential{*expected}, actualList)
+
+	actualList = Credentials{*CredentialFromWebAuthn(expected, true)}.ToWebAuthn(identity.AuthenticatorAssuranceLevel2)
+	assert.Len(t, actualList, 0)
+
+	actualList = Credentials{*CredentialFromWebAuthn(expected, false)}.ToWebAuthn(identity.AuthenticatorAssuranceLevel1)
+	assert.Len(t, actualList, 0)
 
 	fromWebAuthn := CredentialFromWebAuthn(expected, true)
 	assert.True(t, fromWebAuthn.IsPasswordless)
