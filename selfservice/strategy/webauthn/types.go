435bc4f68ecd996db380ce0cf092e8fc62678b36
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-03-02 10:09:15 +0100

    feat(webauthn): add swagger type

---
+++ selfservice/strategy/webauthn/types.go
@@ -0,0 +1,15 @@
+package webauthn
+
+// swagger:model submitSelfServiceFlowWithWebAuthnRegistrationMethod
+type submitSelfServiceFlowWithWebAuthnRegistrationMethod struct {
+	// Register a WebAuthn Security Key
+	//
+	// It is expected that the JSON returned by the WebAuthn registration process
+	// is included here.
+	Register string `json:"webauthn_register"`
+
+	// Name of the WebAuthn Security Key to be Added
+	//
+	// A human-readable name for the security key which will be added.
+	RegisterDisplayName string `json:"webauthn_register_displayname"`
+}
