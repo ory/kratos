4047b67be714f5f74e97a79c80794c212cdfc466
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-03-03 18:48:53 +0100

    test(cmd): fix regressions

--- cmd/identities/get_test.go
+++ cmd/identities/get_test.go
@@ -98,7 +98,7 @@ func TestGetCmd(t *testing.T) {
 		ij, err := json.Marshal(identity.WithCredentialsInJSON(*di))
 		require.NoError(t, err)
 
-		ii := []string{"schema_url", "state_changed_at", "created_at", "updated_at", "credentials.oidc.created_at", "credentials.oidc.updated_at"}
+		ii := []string{"schema_url", "state_changed_at", "created_at", "updated_at", "credentials.oidc.created_at", "credentials.oidc.updated_at", "credentials.oidc.version"}
 		assertx.EqualAsJSONExcept(t, json.RawMessage(ij), json.RawMessage(stdOut), ii)
 	})
 }
