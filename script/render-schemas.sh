e2489740539c6db632cca46a669b0ce2ab5a5055
Anirudh Oppiliappan <x@icyphox.sh>
2022-04-29 11:31:28 +0530

    u

--- script/render-schemas.sh
+++ script/render-schemas.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 ory_x_version="$(go list -f '{{.Version}}' -m github.com/ory/x)"
 
-sed "s!ory://tracing-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/otelx/config.schema.json!g;" embedx/config.schema.json > .schema/config.schema.json
+sed "s!ory://tracing-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/otelx/config.schema.json!g;" embedx/config.schema.json > .schemastore/config.schema.json
 
 git config user.email "60093411+ory-bot@users.noreply.github.com"
 git config user.name "ory-bot"
