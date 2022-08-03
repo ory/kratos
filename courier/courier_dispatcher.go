f0bdd4a66a2c566f9593f398eb4c63d27d3a5cd2
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-08-03 16:52:37 +0200

    chore: move the abandon logic to DispatchMessage

--- courier/courier_dispatcher.go
+++ courier/courier_dispatcher.go
@@ -7,6 +7,32 @@ import (
 )
 
 func (c *courier) DispatchMessage(ctx context.Context, msg Message) error {
+	maxRetries := c.deps.CourierConfig(ctx).CourierMessageRetries()
+
+	if msg.SendCount > maxRetries {
+		if err := c.deps.CourierPersister().SetMessageStatus(ctx, msg.ID, MessageStatusAbandoned); err != nil {
+			c.deps.Logger().
+				WithError(err).
+				WithField("message_id", msg.ID).
+				Error(`Unable to reset the retried message's status to "abandoned".`)
+			return err
+		}
+
+		// Skip the message
+		c.deps.Logger().
+			WithField("message_id", msg.ID).
+			Warnf(`Message was abandoned because it did not deliver after %d tries`, msg.SendCount)
+		return nil
+	}
+
+	if err := c.deps.CourierPersister().IncrementMessageSendCount(ctx, msg.ID); err != nil {
+		c.deps.Logger().
+			WithError(err).
+			WithField("message_id", msg.ID).
+			Error(`Unable to increment the message's "send_count" field`)
+		return err
+	}
+
 	switch msg.Type {
 	case MessageTypeEmail:
 		if err := c.dispatchEmail(ctx, msg); err != nil {
@@ -47,45 +73,22 @@ func (c *courier) DispatchQueue(ctx context.Context) error {
 		return err
 	}
 
-	maxRetries := c.deps.CourierConfig(ctx).CourierMessageRetries()
-
 	for k, msg := range messages {
-		if msg.SendCount > maxRetries {
-			if err := c.deps.CourierPersister().SetMessageStatus(ctx, msg.ID, MessageStatusAbandoned); err != nil {
-				if c.failOnError {
-					return err
-				}
-				c.deps.Logger().
-					WithError(err).
-					WithField("message_id", msg.ID).
-					Error(`Unable to reset the retried message's status to "abandoned".`)
-			}
-		} else {
-			if err := c.deps.CourierPersister().IncrementMessageSendCount(ctx, msg.ID); err != nil {
-				if c.failOnError {
-					return err
-				}
-				c.deps.Logger().
-					WithError(err).
-					WithField("message_id", msg.ID).
-					Error(`Unable to increment the message's "send_count" field`)
-			}
+		if err := c.DispatchMessage(ctx, msg); err != nil {
 
-			if err := c.DispatchMessage(ctx, msg); err != nil {
-				for _, replace := range messages[k:] {
-					if err := c.deps.CourierPersister().SetMessageStatus(ctx, replace.ID, MessageStatusQueued); err != nil {
-						if c.failOnError {
-							return err
-						}
-						c.deps.Logger().
-							WithError(err).
-							WithField("message_id", replace.ID).
-							Error(`Unable to reset the failed message's status to "queued".`)
+			for _, replace := range messages[k:] {
+				if err := c.deps.CourierPersister().SetMessageStatus(ctx, replace.ID, MessageStatusQueued); err != nil {
+					if c.failOnError {
+						return err
 					}
+					c.deps.Logger().
+						WithError(err).
+						WithField("message_id", replace.ID).
+						Error(`Unable to reset the failed message's status to "queued".`)
 				}
-
-				return err
 			}
+
+			return err
 		}
 	}
 
--- courier/courier_dispatcher_test.go
+++ courier/courier_dispatcher_test.go
@@ -5,22 +5,73 @@ import (
 	"testing"
 
 	"github.com/gofrs/uuid"
-	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ory/kratos/courier"
+	"github.com/ory/kratos/courier/template"
 	templates "github.com/ory/kratos/courier/template/email"
 	"github.com/ory/kratos/driver/config"
 	"github.com/ory/kratos/internal"
 )
 
-func TestMessageRetries(t *testing.T) {
+func queueNewMessage(t *testing.T, ctx context.Context, c courier.Courier, d template.Dependencies) uuid.UUID {
+	t.Helper()
+	id, err := c.QueueEmail(ctx, templates.NewTestStub(d, &templates.TestStubModel{
+		To:      "test-recipient-1@example.org",
+		Subject: "test-subject-1",
+		Body:    "test-body-1",
+	}))
+	require.NoError(t, err)
+	return id
+}
+
+func TestDispatchMessageWithInvalidSMTP(t *testing.T) {
+	ctx := context.Background()
+
+	conf, reg := internal.NewRegistryDefaultWithDSN(t, "")
+	conf.MustSet(config.ViperKeyCourierMessageRetries, 5)
+	conf.MustSet(config.ViperKeyCourierSMTPURL, "http://foo.url")
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	c := reg.Courier(ctx)
+
+	t.Run("case=failed sending", func(t *testing.T) {
+		id := queueNewMessage(t, ctx, c, reg)
+		message, err := reg.CourierPersister().LatestQueuedMessage(ctx)
+		require.NoError(t, err)
+		require.Equal(t, id, message.ID)
+
+		err = c.DispatchMessage(ctx, *message)
+		// sending the email fails, because there is no SMTP server at foo.url
+		require.Error(t, err)
+
+		messages, err := reg.CourierPersister().NextMessages(ctx, 10)
+		require.Len(t, messages, 1)
+	})
+
+	t.Run("case=max retries reached", func(t *testing.T) {
+		id := queueNewMessage(t, ctx, c, reg)
+		message, err := reg.CourierPersister().LatestQueuedMessage(ctx)
+		require.NoError(t, err)
+		require.Equal(t, id, message.ID)
+		message.SendCount = 6
+
+		err = c.DispatchMessage(ctx, *message)
+		require.NoError(t, err)
+
+		messages, err := reg.CourierPersister().NextMessages(ctx, 1)
+		require.Empty(t, messages)
+	})
+
+}
+
+func TestDispatchMessage2(t *testing.T) {
 	ctx := context.Background()
 
 	conf, reg := internal.NewRegistryDefaultWithDSN(t, "")
 	conf.MustSet(config.ViperKeyCourierMessageRetries, 1)
-
-	reg.Logger().Level = logrus.TraceLevel
 
 	c := reg.Courier(ctx)
 
@@ -39,7 +90,7 @@ func TestMessageRetries(t *testing.T) {
 	err = c.DispatchQueue(ctx)
 	require.Error(t, err)
 
-	// Retry once, as we set above
+	// Retry once, as we set above - still fails
 	err = c.DispatchQueue(ctx)
 	require.Error(t, err)
 
