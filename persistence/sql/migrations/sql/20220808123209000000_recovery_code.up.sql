744c148e6cc1b52af3f3b41a6192c6754110311b
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-08-09 16:52:47 +0200

    fix: typos in SQL migration

--- persistence/sql/migrations/sql/20220808123209000000_recovery_code.up.sql
+++ persistence/sql/migrations/sql/20220808123209000000_recovery_code.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE "identity_recovery_codes"
 (
     "id" UUID NOT NULL PRIMARY KEY,
-    "code" VARCHAR (8) NOT NULL,
+    "code" VARCHAR (64) NOT NULL, -- HMACed value of the actual code
     "used" bool NOT NULL DEFAULT 'false',
     "used_at" timestamp,
-    "identity_recovery_address_id" UUID NOT NULL,
+    "identity_recovery_address_id" UUID,
     "expires_at" timestamp NOT NULL DEFAULT '2000-01-01 00:00:00',
     "issued_at" timestamp NOT NULL DEFAULT '2000-01-01 00:00:00',
-    "selfservice_recovery_flow_id" UUID NOT NULL,
+    "selfservice_recovery_flow_id" UUID,
     "created_at" timestamp NOT NULL,
     "updated_at" timestamp NOT NULL,
     "nid" UUID NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE "identity_recovery_codes"
     CONSTRAINT "identity_recovery_codes_selfservice_recovery_flows_id_fk" 
         FOREIGN KEY ("selfservice_recovery_flow_id") 
         REFERENCES "selfservice_recovery_flows" ("id")
-        ON DELETE cascade
+        ON DELETE cascade,
     CONSTRAINT "identity_recovery_tokens_identity_id_fk" 
         FOREIGN KEY ("identity_id") 
         REFERENCES "identities" ("id")
