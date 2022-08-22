8cd7e7247aab5caf57f83433d0e19503a3329b96
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-08-22 18:53:44 +0200

    fix: cockroach db migration

---
+++ persistence/sql/migrations/sql/20220819132309000000_add_flow_type_to_identity_recovery_tokens.cockroach.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE identity_recovery_tokens
+ADD token_type int;
+
+COMMIT TRANSACTION;BEGIN TRANSACTION;
+
+UPDATE identity_recovery_tokens
+SET token_type = 1
+WHERE selfservice_recovery_flow_id IS NULL;
