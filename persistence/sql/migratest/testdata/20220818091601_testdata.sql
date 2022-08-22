6e921249b6ba32e11c4d01f09c5173d966f2aceb
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-08-22 19:22:09 +0200

    chore: fix quote type in testdata

--- persistence/sql/migratest/testdata/20220818091601_testdata.sql
+++ persistence/sql/migratest/testdata/20220818091601_testdata.sql
@@ -1,15 +1,15 @@
 INSERT INTO identity_recovery_codes (id, code, used, used_at, identity_recovery_address_id, code_type, expires_at, issued_at, selfservice_recovery_flow_id, created_at, updated_at, nid, identity_id) VALUES (
-    "8f75f5d9-9cb4-4848-9a73-9344f686f8a6",
-    "7eb71370d8497734ec78dfe613bf0f08967e206d2b5c2fc1243be823cfcd57a7",
+    '8f75f5d9-9cb4-4848-9a73-9344f686f8a6',
+    '7eb71370d8497734ec78dfe613bf0f08967e206d2b5c2fc1243be823cfcd57a7',
     false,
     null,
-    "b8293f1c-010f-45d9-b809-f3fc5365ba80",
+    'b8293f1c-010f-45d9-b809-f3fc5365ba80',
     1,
-    "2022-08-18 08:28:18.470717",
-    "2022-08-18 07:28:18.470717",
-    "68fb4010-84a9-4d1e-9f92-2705978ee89e",
-    "2022-08-18 07:28:18.470789",
-    "2022-08-18 07:28:18.470789",
-    "b64ad553-8107-42cb-a350-7087fbdb5387",
-    "308929d3-41a2-43fe-a33c-75308539d841"
+    '2022-08-18 08:28:18.470717',
+    '2022-08-18 07:28:18.470717',
+    '68fb4010-84a9-4d1e-9f92-2705978ee89e',
+    '2022-08-18 07:28:18.470789',
+    '2022-08-18 07:28:18.470789',
+    'b64ad553-8107-42cb-a350-7087fbdb5387',
+    '308929d3-41a2-43fe-a33c-75308539d841'
 )
