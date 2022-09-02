b63e6d1d6cae455711b1866ee9fc47e86e50a43c
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-09-02 15:42:40 +0200

    fix(e2e): ui login error message

--- test/e2e/cypress/integration/profiles/email/login/error.spec.ts
+++ test/e2e/cypress/integration/profiles/email/login/error.spec.ts
@@ -42,8 +42,14 @@ describe("Basic email profile with failing login flows", () => {
         )
       })
 
-      describe("shows validation errors when invalid signup data is used", () => {
-        it("should show an error when the identifier is missing", () => {
+      describe('shows validation errors when invalid signup data is used', () => {
+        it('should show an error when the identifier is missing', () => {
+          // the browser will prevent the form from submitting if the fields are empty since they are required
+          // here we just remove the required attribute to make the form submit
+          cy.removeRequiredAttribute([
+            'input[name="identifier"]',
+            'input[name="password"]'
+          ])
           cy.submitPasswordForm()
           cy.get('*[data-testid="ui/message/4000002"]').should(
             "contain.text",
@@ -61,13 +67,17 @@ describe("Basic email profile with failing login flows", () => {
             .type(identity)
             .should("have.value", identity)
 
+          // the browser will prevent the form from submitting if the fields are empty since they are required
+          // here we just remove the required attribute to make the form submit
+          cy.removeRequiredAttribute(['input[name="password"]'])
+
           cy.submitPasswordForm()
           cy.get('*[data-testid^="ui/message/"]')
             .invoke("text")
             .then((text) => {
-              expect(text).to.be.oneOf([
-                "length must be >= 1, but got 0",
-                "Property password is missing.",
+              expect(text.trim()).to.be.oneOf([
+                'length must be >= 1, but got 0',
+                'Property password is missing.'
               ])
             })
         })
