b5477aad994bafdb682f362595ae021b697f2164
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-09-04 15:20:46 +0200

    test: add repro

--- identity/handler_test.go
+++ identity/handler_test.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
@@ -642,6 +643,55 @@ func TestHandler(t *testing.T) {
 			}
 		})
 
+		t.Run("case=PATCH should not invalidate credentials ory/cloud#148", func(t *testing.T) {
+			// see https://github.com/ory/cloud/issues/148
+
+			createCredentials := func(t *testing.T) (*identity.Identity, string, string) {
+				t.Helper()
+				uuid := x.NewUUID().String()
+				email := uuid + "@ory.sh"
+				password := "ljanf123akf"
+				p, err := reg.Hasher(ctx).Generate(context.Background(), []byte(password))
+				require.NoError(t, err)
+				i := &identity.Identity{Traits: identity.Traits(`{"email":"` + email + `"}`)}
+				i.SetCredentials(identity.CredentialsTypePassword, identity.Credentials{
+					Type:        identity.CredentialsTypePassword,
+					Identifiers: []string{email},
+					Config:      sqlxx.JSONRawMessage(`{"hashed_password":"` + string(p) + `"}`),
+				})
+				require.NoError(t, reg.PrivilegedIdentityPool().CreateIdentity(context.Background(), i))
+				return i, email, password
+			}
+
+			for name, ts := range map[string]*httptest.Server{"public": publicTS, "admin": adminTS} {
+				t.Run("endpoint="+name, func(t *testing.T) {
+					i, email, password := createCredentials(t)
+					values := func(v url.Values) {
+						v.Set("identifier", email)
+						v.Set("password", password)
+					}
+
+					// verify login works initially
+					loginResponse := testhelpers.SubmitLoginForm(t, true, ts.Client(), ts, values, false, true, 200, "")
+					require.NotEmpty(t, gjson.Get(loginResponse, "session_token").String(), "expected to find a session token, found none")
+
+					ur := makePatch(t, patch{
+						"op": "replace", "path": "/metadata_public", "value": map[string]string{
+							"role": "user",
+						},
+					})
+
+					res := send(t, ts, "PATCH", "/identities/"+i.ID.String(), http.StatusOK, &ur)
+					assert.EqualValues(t, "user", res.Get("metadata_public.role").String(), "%s", res.Raw)
+					assert.NotEqualValues(t, i.StateChangedAt, sqlxx.NullTime(res.Get("state_changed_at").Time()), "%s", res.Raw)
+
+					loginResponse = testhelpers.SubmitLoginForm(t, true, ts.Client(), ts, values, false, true, 200, "")
+					msgs := gjson.Get(loginResponse, "ui.messages")
+					require.Empty(t, msgs.Array(), "expected to find no messages: %s", msgs.String())
+				})
+			}
+		})
+
 		t.Run("case=should delete a user and no longer be able to retrieve it", func(t *testing.T) {
 			for name, ts := range map[string]*httptest.Server{"public": publicTS, "admin": adminTS} {
 				t.Run("endpoint="+name, func(t *testing.T) {
@@ -842,11 +892,15 @@ func TestHandler(t *testing.T) {
 					cr.Traits = []byte(`{"department": "ory"}`)
 					res := send(t, ts, "POST", "/identities", http.StatusCreated, &cr)
 
+					assert.Empty(t, res.Get("credentials").Array())
+
 					id := res.Get("id").String()
 					res = send(t, ts, "PUT", "/identities/"+id, http.StatusOK, &identity.AdminUpdateIdentityBody{
 						SchemaID: "employee",
 						Traits:   []byte(`{"email":"` + x.NewUUID().String() + `@ory.sh"}`),
 					})
+
+					assert.Empty(t, res.Get("credentials").Array())
 
 					res = send(t, ts, "PUT", "/identities/"+id, http.StatusOK, &identity.AdminUpdateIdentityBody{
 						SchemaID: "employee",
