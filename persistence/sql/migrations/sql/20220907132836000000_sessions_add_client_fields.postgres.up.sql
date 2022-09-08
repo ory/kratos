0d7bfc7f3b7e080362568bafa00c99d452436446
Ajay Kelkar <ajay.kelkar@ory.sh>
2022-09-08 16:06:30 +0200

    fix: specify default values for all db flavours

--- persistence/sql/migrations/sql/20220907132836000000_sessions_add_client_fields.postgres.up.sql
+++ persistence/sql/migrations/sql/20220907132836000000_sessions_add_client_fields.postgres.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE sessions
-  ADD COLUMN client_ip_address VARCHAR(50),
-  ADD COLUMN user_agent TEXT,
-  ADD COLUMN geo_location TEXT;
+  ADD COLUMN client_ip_address VARCHAR(50) DEFAULT '',
+  ADD COLUMN user_agent TEXT DEFAULT '',
+  ADD COLUMN geo_location TEXT DEFAULT '';
