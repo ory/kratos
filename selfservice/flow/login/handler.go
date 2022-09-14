1f2f6503ae9680b49cb5ff5f67bd571f042b6d84
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-09-14 10:15:12 +0200

    fix: remove debug info from error and trace errors

--- selfservice/flow/login/handler.go
+++ selfservice/flow/login/handler.go
@@ -347,7 +347,7 @@ type initializeSelfServiceLoginFlowForBrowsers struct {
 // - `security_identity_mismatch`: The requested `?return_to` address is not allowed to be used. Adjust this in the configuration!
 //
 // The optional query parameter login_challenge is set when using Kratos with
-// Hydra in an OAUTH flow. See the selfservice.hydra_admin_url configuration
+// Hydra in an OAuth2 flow. See the selfservice.hydra_admin_url configuration
 // option.
 //
 // This endpoint is NOT INTENDED for clients that do not have a browser (Chrome, Firefox, ...) as cookies are needed.
