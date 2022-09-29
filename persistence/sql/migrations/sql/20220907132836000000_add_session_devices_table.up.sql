df188efad0db5b46f1d9784e9ebe6db869157e49
Ajay Kelkar <ajay.kelkar@ory.sh>
2022-09-29 15:31:46 +0200

    refactor: condense migrations

--- persistence/sql/migrations/sql/20220907132836000000_add_session_devices_table.up.sql
+++ persistence/sql/migrations/sql/20220907132836000000_add_session_devices_table.up.sql
@@ -1,14 +1,13 @@
 CREATE TABLE "session_devices"
 (
-  "id"         UUID      NOT NULL,
-  PRIMARY KEY ("id"),
-  "ip_address" STRING(50) DEFAULT '',
-  "user_agent" STRING(512) DEFAULT '',
-  "location"   STRING(512) DEFAULT '',
-  "session_id" UUID      NOT NULL,
-  "nid"        UUID      NOT NULL,
-  "created_at" timestamp NOT NULL,
-  "updated_at" timestamp NOT NULL,
+  "id"         UUID PRIMARY KEY NOT NULL,
+  "ip_address" VARCHAR(50)  DEFAULT '',
+  "user_agent" VARCHAR(512) DEFAULT '',
+  "location"   VARCHAR(512) DEFAULT '',
+  "nid"        UUID             NOT NULL,
+  "session_id" UUID             NOT NULL,
+  "created_at" timestamp        NOT NULL,
+  "updated_at" timestamp        NOT NULL,
   CONSTRAINT "session_metadata_sessions_id_fk" FOREIGN KEY ("session_id") REFERENCES "sessions" ("id") ON DELETE cascade,
   CONSTRAINT "session_metadata_nid_fk" FOREIGN KEY ("nid") REFERENCES "networks" ("id") ON DELETE cascade,
   CONSTRAINT unique_session_device UNIQUE (nid, session_id, ip_address, user_agent)
