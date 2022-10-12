901a7f9f51408b805c2104460b4311a34d21d0cf
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2022-10-12 15:03:24 +0200

    fix: formatting

--- selfservice/strategy/link/sender_test.go
+++ selfservice/strategy/link/sender_test.go
@@ -7,8 +7,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ory/kratos/internal/testhelpers"
 	"github.com/pkg/errors"
+
+	"github.com/ory/kratos/internal/testhelpers"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
