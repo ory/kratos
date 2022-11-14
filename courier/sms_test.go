97cad419dd0ee728538f7e7fd38d19d7e400440d
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-11-14 14:21:24 +0100

    fix: do not crash process on invalid smtp url
    Closes https://github.com/ory-corp/cloud/issues/3321

--- courier/sms_test.go
+++ courier/sms_test.go
@@ -86,7 +86,8 @@ func TestQueueSMS(t *testing.T) {
 	conf.MustSet(ctx, config.ViperKeyCourierSMTPURL, "http://foo.url")
 	reg.Logger().Level = logrus.TraceLevel
 
-	c := reg.Courier(ctx)
+	c, err := reg.Courier(ctx)
+	require.NoError(t, err)
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer t.Cleanup(cancel)
@@ -132,11 +133,12 @@ func TestDisallowedInternalNetwork(t *testing.T) {
 	conf.MustSet(ctx, config.ViperKeyClientHTTPNoPrivateIPRanges, true)
 	reg.Logger().Level = logrus.TraceLevel
 
-	c := reg.Courier(ctx)
+	c, err := reg.Courier(ctx)
+	require.NoError(t, err)
 	c.(interface {
 		FailOnDispatchError()
 	}).FailOnDispatchError()
-	_, err := c.QueueSMS(ctx, sms.NewTestStub(reg, &sms.TestStubModel{
+	_, err = c.QueueSMS(ctx, sms.NewTestStub(reg, &sms.TestStubModel{
 		To:   "+12065550101",
 		Body: "test-sms-body-1",
 	}))
