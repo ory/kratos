f7fe7512f7f79d3a6db21d53e905d51691076ddf
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2023-01-06 16:44:29 +0100

    chore: fix list courier messages index

--- persistence/sql/migrations/sql/20230104193739000000_courier_list_index.up.sql
+++ persistence/sql/migrations/sql/20230104193739000000_courier_list_index.up.sql
@@ -1,3 +1,3 @@
-CREATE INDEX courier_messages_status_idx ON courier_messages (status);
+CREATE INDEX courier_messages_nid_created_at_id_idx ON courier_messages (nid, created_at, id);
 
-CREATE INDEX courier_messages_status_idx ON courier_messages (recipient);
+CREATE INDEX courier_messages_recipient_at_id_idx ON courier_messages (recipient);
