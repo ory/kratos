cb66eafee985b7c7e809db989845713792cd2845
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2023-01-07 20:02:45 +0100

    chore: fix index ordering

--- persistence/sql/migrations/sql/20230104193739000000_courier_list_index.up.sql
+++ persistence/sql/migrations/sql/20230104193739000000_courier_list_index.up.sql
@@ -1,3 +1,3 @@
-CREATE INDEX courier_messages_nid_created_at_id_idx ON courier_messages (nid, created_at, id);
+CREATE INDEX courier_messages_nid_created_at_id_idx ON courier_messages (nid, created_at DESC, id);
 
 CREATE INDEX courier_messages_recipient_idx ON courier_messages (recipient);
