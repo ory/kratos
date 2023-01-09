aaf49e562a32840e932dad9453df57571783ce56
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2023-01-09 17:31:56 +0100

    chore: add status and recipient indices

--- persistence/sql/migrations/sql/20230104193739000000_courier_list_index.down.sql
+++ persistence/sql/migrations/sql/20230104193739000000_courier_list_index.down.sql
@@ -1,3 +1,5 @@
 DROP INDEX courier_messages_nid_created_at_id_idx;
 
-DROP INDEX courier_messages_recipient_idx;
+DROP INDEX courier_messages_nid_status_created_at_id_idx;
+
+DROP INDEX courier_messages_nid_recipient_created_at_id_idx;
