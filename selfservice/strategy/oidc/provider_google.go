387b5ee0e32d5c7d1d2e9ea6365b3da8ddf6590c
tsearle <torrey@openline.ai>
2023-02-09 15:37:30 +0100

    implement offline scope in the way google expects

--- selfservice/strategy/oidc/provider_google.go
+++ selfservice/strategy/oidc/provider_google.go
@@ -3,6 +3,13 @@
 
 package oidc
 
+import (
+	"context"
+	gooidc "github.com/coreos/go-oidc"
+	"github.com/ory/x/stringslice"
+	"golang.org/x/oauth2"
+)
+
 type ProviderGoogle struct {
 	*ProviderGenericOIDC
 }
@@ -18,4 +25,37 @@ func NewProviderGoogle(
 			reg:    reg,
 		},
 	}
+}
+
+func (g *ProviderGoogle) oauth2ConfigFromEndpoint(ctx context.Context, endpoint oauth2.Endpoint) *oauth2.Config {
+	scope := g.config.Scope
+	if !stringslice.Has(scope, gooidc.ScopeOpenID) {
+		scope = append(scope, gooidc.ScopeOpenID)
+	}
+
+	return &oauth2.Config{
+		ClientID:     g.config.ClientID,
+		ClientSecret: g.config.ClientSecret,
+		Endpoint:     endpoint,
+		Scopes:       stringslice.Filter(scope, func(s string) bool { return s == gooidc.ScopeOfflineAccess }),
+		RedirectURL:  g.config.Redir(g.reg.Config().OIDCRedirectURIBase(ctx)),
+	}
+}
+
+func (g *ProviderGoogle) AuthCodeURLOptions(r ider) []oauth2.AuthCodeOption {
+	var options []oauth2.AuthCodeOption
+	scope := g.config.Scope
+
+	if isForced(r) {
+		options = append(options, oauth2.SetAuthURLParam("prompt", "login"))
+	}
+	if len(g.config.RequestedClaims) != 0 {
+		options = append(options, oauth2.SetAuthURLParam("claims", string(g.config.RequestedClaims)))
+	}
+
+	if !stringslice.Has(scope, gooidc.ScopeOfflineAccess) {
+		options = append(options, oauth2.AccessTypeOffline)
+	}
+
+	return options
 }
