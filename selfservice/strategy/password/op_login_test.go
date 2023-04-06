bc95ce26843e18cbbaefe1c0450cc85e5617ac39
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-04-06 12:51:28 +0200

    fix: TestOAuth2Provider

--- selfservice/strategy/password/op_login_test.go
+++ selfservice/strategy/password/op_login_test.go
@@ -163,6 +163,7 @@ func TestOAuth2Provider(t *testing.T) {
 		config.ViperKeySelfServiceStrategyConfig+"."+string(identity.CredentialsTypePassword),
 		map[string]interface{}{"enabled": true},
 	)
+
 	router := x.NewRouterPublic()
 	kratosPublicTS, _ := testhelpers.NewKratosServerWithRouters(t, reg, router, x.NewRouterAdmin())
 
@@ -238,6 +239,11 @@ func TestOAuth2Provider(t *testing.T) {
 
 	hydraAdmin, hydraPublic := newHydra(t, uiTS.URL, uiTS.URL)
 	conf.MustSet(ctx, config.ViperKeyOAuth2ProviderURL, hydraAdmin)
+
+	// Kratos automatically adds the request URL from Hydra to the `return_to` parameter.
+	// We need to allow this URL in the config.
+	reg.Config().MustSet(ctx, config.ViperKeyURLsAllowedReturnToDomains, []string{hydraPublic})
+
 	hydraAdminClient = createHydraOAuth2ApiClient(hydraAdmin)
 	clientID := createOAuth2Client(t, ctx, hydraAdminClient, []string{clientAppTS.URL}, "profile email")
 
