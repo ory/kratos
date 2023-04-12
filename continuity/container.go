ef211e23dca25babd12462ba4a8c86a4f79d4fa7
Arne Luenser <arne.luenser@ory.sh>
2023-04-12 15:50:07 +0200

    chore: delete x.DerefUUID

--- continuity/container.go
+++ continuity/container.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/ory/herodot"
+	"github.com/ory/x/pointerx"
 	"github.com/ory/x/sqlxx"
 
 	"github.com/ory/kratos/x"
@@ -61,7 +62,7 @@ func (c *Container) Valid(identity uuid.UUID) error {
 		return errors.WithStack(herodot.ErrBadRequest.WithReasonf("You must restart the flow because the resumable session has expired."))
 	}
 
-	if identity != uuid.Nil && x.DerefUUID(c.IdentityID) != identity {
+	if identity != uuid.Nil && pointerx.Deref(c.IdentityID) != identity {
 		return errors.WithStack(herodot.ErrBadRequest.WithReasonf("You must restart the flow because the resumable session was initiated by another person."))
 	}
 
--- x/pointer.go
+++ x/pointer.go
@@ -11,10 +11,3 @@ func PointToUUID(id uuid.UUID) *uuid.UUID {
 	}
 	return &id
 }
-
-func DerefUUID(id *uuid.UUID) uuid.UUID {
-	if id == nil {
-		return uuid.Nil
-	}
-	return *id
-}
