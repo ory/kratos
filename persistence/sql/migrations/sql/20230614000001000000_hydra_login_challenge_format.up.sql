5871f812e06b9c19edff47d437196dca813d9bc6
Henning Perl <henning.perl@ory.sh>
2023-06-19 10:41:09 +0200

    fix: defer dropping the old column

--- persistence/sql/migrations/sql/20230614000001000000_hydra_login_challenge_format.up.sql
+++ persistence/sql/migrations/sql/20230614000001000000_hydra_login_challenge_format.up.sql
@@ -1,5 +1,6 @@
-ALTER TABLE selfservice_login_flows DROP COLUMN oauth2_login_challenge;
-ALTER TABLE selfservice_registration_flows DROP COLUMN oauth2_login_challenge;
+-- Drop columns in a later migration.
+-- ALTER TABLE selfservice_login_flows DROP COLUMN oauth2_login_challenge;
+-- ALTER TABLE selfservice_registration_flows DROP COLUMN oauth2_login_challenge;
 
 ALTER TABLE selfservice_login_flows ADD COLUMN oauth2_login_challenge_data TEXT NULL;
 ALTER TABLE selfservice_registration_flows ADD COLUMN oauth2_login_challenge_data TEXT NULL;
