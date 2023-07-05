2576dd2134958569d7e281bf4bca4c8a8f354c01
aeneasr <3372410+aeneasr@users.noreply.github.com>
2023-07-05 17:33:52 +0200

    u

--- persistence/sql/migrations/sql/20230705000000000001_cookie_flow_request_url.down.sql
+++ persistence/sql/migrations/sql/20230705000000000001_cookie_flow_request_url.down.sql
@@ -1,3 +1,9 @@
+UPDATE selfservice_login_flows SET request_url = substr(request_url, 1, 1024);
+UPDATE selfservice_recovery_flows SET request_url = substr(request_url, 1, 1024);
+UPDATE selfservice_registration_flows SET request_url = substr(request_url, 1, 1024);
+UPDATE selfservice_settings_flows SET request_url = substr(request_url, 1, 1024);
+UPDATE selfservice_verification_flows SET request_url = substr(request_url, 1, 1024);
+
 ALTER TABLE selfservice_login_flows ALTER COLUMN request_url TYPE VARCHAR(1024);
 ALTER TABLE selfservice_recovery_flows ALTER COLUMN request_url TYPE VARCHAR(1024);
 ALTER TABLE selfservice_registration_flows ALTER COLUMN request_url TYPE VARCHAR(1024);
