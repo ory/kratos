859003f11fb5ed19565b354155aad588c5afbace
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2023-08-17 10:35:07 +0200

    chore: fix assertions

--- test/e2e/cypress/integration/profiles/mobile/mfa/backup.spec.ts
+++ test/e2e/cypress/integration/profiles/mobile/mfa/backup.spec.ts
@@ -4,7 +4,7 @@
 import { gen, MOBILE_URL, website } from "../../../../helpers"
 
 context("Mobile Profile", () => {
-  describe("TOTP 2FA Flow", () => {
+  describe.only("TOTP 2FA Flow", () => {
     before(() => {
       cy.useConfigProfile("mobile")
     })
@@ -76,15 +76,22 @@ context("Mobile Profile", () => {
         cy.get(
           '[data-testid="field/lookup_secret_regenerate/true"]:disabled',
         ).should("not.exist")
-        cy.get('[data-testid="field/lookup_secret_codes/text"]').then(($e) => {
-          newCodes = $e.text().trim().split(", ")
-          expect(newCodes.join(", ")).to.not.eq(codes.join(", "))
-        })
+
+        cy.get('[data-testid="field/lookup_secret_codes/text"]')
+          .and(($e) => {
+            expect($e.text().trim().split(", ").join(", ")).to.not.eq(
+              codes.join(", "),
+            )
+          })
+          .then(($e) => {
+            newCodes = $e.text().trim().split(", ")
+          })
+
         cy.get('[data-testid="field/lookup_secret_confirm/true"]').click()
         cy.expectSettingsSaved()
 
         cy.get('[data-testid="field/lookup_secret_reveal/true"]').click()
-        cy.get('[data-testid="field/lookup_secret_codes/text"]').then(($e) => {
+        cy.get('[data-testid="field/lookup_secret_codes/text"]').and(($e) => {
           const actualCodes = $e.text().trim().split(", ")
           expect(actualCodes.join(", ")).to.eq(newCodes.join(", "))
         })
