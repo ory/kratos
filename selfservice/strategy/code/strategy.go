33da466074a7165daebf240e9fcf8a8b3035bf46
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-09-28 11:50:47 +0200

    fix: identifier label on login

--- selfservice/strategy/code/strategy.go
+++ selfservice/strategy/code/strategy.go
@@ -150,9 +150,7 @@ func (s *Strategy) PopulateMethod(r *http.Request, f flow.Flow) error {
 		} else if f.GetFlowName() == flow.LoginFlow {
 			// we use the identifier label here since we don't know what
 			// type of field the identifier is
-			nodes.Upsert(
-				node.NewInputField("identifier", nil, node.DefaultGroup, node.InputAttributeTypeText, node.WithRequiredInputAttribute),
-			)
+			nodes.Upsert(node.NewInputField("identifier", "", node.DefaultGroup, node.InputAttributeTypeText, node.WithRequiredInputAttribute).WithMetaLabel(text.NewInfoNodeLabelID()))
 		} else if f.GetFlowName() == flow.RegistrationFlow {
 			ds, err := s.deps.Config().DefaultIdentityTraitsSchemaURL(r.Context())
 			if err != nil {
