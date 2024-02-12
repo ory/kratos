83a23b0f1251d1458e7e85276ffa81f89903d4b7
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-02-12 13:03:34 +0100

    chore: code review
    Also renames the settings for two step registration

--- identity/credentials_webauthn_test.go
+++ identity/credentials_webauthn_test.go
@@ -4,6 +4,7 @@
 package identity
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/go-webauthn/webauthn/webauthn"
@@ -46,4 +47,17 @@ func TestCredentialConversion(t *testing.T) {
 	expected.Authenticator.AAGUID = uuid.Must(uuid.FromString("ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4")).Bytes()
 	fromWebAuthn = CredentialFromWebAuthn(expected, false)
 	assert.Equal(t, "Google Password Manager", fromWebAuthn.DisplayName)
+}
+
+func TestPasswordlessOnly(t *testing.T) {
+	a := *CredentialFromWebAuthn(&webauthn.Credential{ID: []byte("a")}, false)
+	b := *CredentialFromWebAuthn(&webauthn.Credential{ID: []byte("b")}, false)
+	c := *CredentialFromWebAuthn(&webauthn.Credential{ID: []byte("c")}, true)
+	d := *CredentialFromWebAuthn(&webauthn.Credential{ID: []byte("d")}, false)
+	e := *CredentialFromWebAuthn(&webauthn.Credential{ID: []byte("e")}, true)
+	expected := CredentialsWebAuthn{a, b, c, d, e}
+
+	actual := expected.PasswordlessOnly()
+	require.Len(t, actual, 2)
+	assert.Equal(t, []webauthn.Credential{*c.ToWebAuthn(), *e.ToWebAuthn()}, actual)
 }
