3e769e283dee0dc15d6dc8fa13fe941435baa558
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-02-21 16:14:02 +0100

    chore: synchronize workspaces

--- persistence/sql/migrations/sql/20240214113828000000_courier_dispatch_indices.down.sql
+++ persistence/sql/migrations/sql/20240214113828000000_courier_dispatch_indices.down.sql
@@ -1,4 +1,4 @@
-CREATE INDEX courier_message_dispatches_id_message_id_nid_idx ON courier_message_dispatches (id ASC, message_id ASC, nid ASC);
+CREATE INDEX IF NOT EXISTS courier_message_dispatches_id_message_id_nid_idx ON courier_message_dispatches (id ASC, message_id ASC, nid ASC);
 
 DROP INDEX courier_message_dispatches_message_id_idx;
 DROP INDEX courier_message_dispatches_message_nid_idx;
