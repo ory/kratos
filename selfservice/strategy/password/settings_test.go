7191623c724523c7a1b70a13a076d5ff236ead76
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-03-01 13:13:40 +0100

    chore: synchronize workspaces

--- selfservice/strategy/password/settings_test.go
+++ selfservice/strategy/password/settings_test.go
@@ -203,7 +203,7 @@ func TestSettings(t *testing.T) {
 			values.Set("password", x.NewUUID().String())
 			actual, res := testhelpers.SettingsMakeRequest(t, true, false, f, apiUser2, testhelpers.EncodeFormAsJSON(t, true, values))
 			assert.Equal(t, http.StatusForbidden, res.StatusCode)
-			assert.Contains(t, gjson.Get(actual, "error.reason").String(), "initiated by another person", "%s", actual)
+			assert.Contains(t, gjson.Get(actual, "error.reason").String(), "initiated by someone else", "%s", actual)
 		})
 
 		t.Run("type=spa", func(t *testing.T) {
@@ -213,7 +213,7 @@ func TestSettings(t *testing.T) {
 			values.Set("password", x.NewUUID().String())
 			actual, res := testhelpers.SettingsMakeRequest(t, false, true, f, browserUser2, values.Encode())
 			assert.Equal(t, http.StatusForbidden, res.StatusCode)
-			assert.Contains(t, gjson.Get(actual, "error.reason").String(), "initiated by another person", "%s", actual)
+			assert.Contains(t, gjson.Get(actual, "error.reason").String(), "initiated by someone else", "%s", actual)
 		})
 
 		t.Run("type=browser", func(t *testing.T) {
@@ -223,7 +223,7 @@ func TestSettings(t *testing.T) {
 			values.Set("password", x.NewUUID().String())
 			actual, res := testhelpers.SettingsMakeRequest(t, false, false, f, browserUser2, values.Encode())
 			assert.Equal(t, http.StatusForbidden, res.StatusCode)
-			assert.Contains(t, gjson.Get(actual, "error.reason").String(), "initiated by another person", "%s", actual)
+			assert.Contains(t, gjson.Get(actual, "error.reason").String(), "initiated by someone else", "%s", actual)
 		})
 	})
 
