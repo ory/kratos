9222eb3f441ac63075fb0a6a56259cdc2473ed91
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-06-17 13:36:07 +0200

    test: add tests for idfirst

--- selfservice/strategy/idfirst/strategy_login_test.go
+++ selfservice/strategy/idfirst/strategy_login_test.go
@@ -4,11 +4,27 @@
 package idfirst_test
 
 import (
+	"bytes"
 	"context"
+	_ "embed"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
+
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
+
+	kratos "github.com/ory/kratos/internal/httpclient"
+	"github.com/ory/kratos/text"
+	"github.com/ory/kratos/x"
+	"github.com/ory/x/assertx"
+	"github.com/ory/x/ioutilx"
+	"github.com/ory/x/urlx"
 
 	"github.com/stretchr/testify/require"
 
@@ -21,6 +37,363 @@ import (
 	"github.com/ory/kratos/ui/node"
 	"github.com/ory/x/snapshotx"
 )
+
+//go:embed stub/default.schema.json
+var loginSchema []byte
+
+func TestCompleteLogin(t *testing.T) {
+	ctx := context.Background()
+	conf, reg := internal.NewFastRegistryWithMocks(t)
+
+	// We enable the password method to test the identifier first strategy
+
+	//ctx = config.WithConfigValue(ctx, config.ViperKeySelfServiceStrategyConfig+"."+string(identity.CredentialsTypePassword), map[string]interface{}{"enabled": true})
+	conf.MustSet(ctx, config.ViperKeySelfServiceStrategyConfig+"."+string(identity.CredentialsTypePassword), map[string]interface{}{"enabled": true})
+
+	//ctx = config.WithConfigValue(ctx, config.ViperKeySelfServiceLoginFlowStyle, "identifier_first")
+	conf.MustSet(ctx, config.ViperKeySelfServiceLoginFlowStyle, "identifier_first")
+
+	router := x.NewRouterPublic()
+	publicTS, _ := testhelpers.NewKratosServerWithRouters(t, reg, router, x.NewRouterAdmin())
+
+	errTS := testhelpers.NewErrorTestServer(t, reg)
+	uiTS := testhelpers.NewLoginUIFlowEchoServer(t, reg)
+	redirTS := testhelpers.NewRedirSessionEchoTS(t, reg)
+
+	// Overwrite these two:
+	//ctx = config.WithConfigValue(ctx, config.ViperKeySelfServiceErrorUI, errTS.URL+"/error-ts")
+	conf.MustSet(ctx, config.ViperKeySelfServiceErrorUI, errTS.URL+"/error-ts")
+
+	//ctx = config.WithConfigValue(ctx, config.ViperKeySelfServiceLoginUI, uiTS.URL+"/login-ts")
+	conf.MustSet(ctx, config.ViperKeySelfServiceLoginUI, uiTS.URL+"/login-ts")
+
+	//ctx = testhelpers.WithDefaultIdentitySchemaFromRaw(ctx, loginSchema)
+	testhelpers.SetDefaultIdentitySchemaFromRaw(conf, loginSchema)
+
+	//ctx = config.WithConfigValue(ctx, config.ViperKeySecretsDefault, []string{"not-a-secure-session-key"})
+	conf.MustSet(ctx, config.ViperKeySecretsDefault, []string{"not-a-secure-session-key"})
+
+	//ensureFieldsExist := func(t *testing.T, body []byte) {
+	//	registrationhelpers.CheckFormContent(t, body, "identifier",
+	//		"password",
+	//		"csrf_token")
+	//}
+
+	apiClient := testhelpers.NewDebugClient(t)
+
+	t.Run("case=should show the error ui because the request payload is malformed", func(t *testing.T) {
+		t.Run("type=api", func(t *testing.T) {
+			f := testhelpers.InitializeLoginFlowViaAPIWithContext(t, ctx, apiClient, publicTS, false)
+
+			body, res := testhelpers.LoginMakeRequestWithContext(t, ctx, true, false, f, apiClient, "14=)=!(%)$/ZP()GHIÖ")
+			assert.Contains(t, res.Request.URL.String(), publicTS.URL+login.RouteSubmitFlow)
+			assert.NotEmpty(t, gjson.Get(body, "id").String(), "%s", body)
+			assert.Contains(t, body, `Expected JSON sent in request body to be an object but got: Number`)
+		})
+
+		t.Run("type=browser", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+			f := testhelpers.InitializeLoginFlowViaBrowser(t, browserClient, publicTS, false, false, false, false, testhelpers.InitFlowWithContext(ctx))
+
+			body, res := testhelpers.LoginMakeRequestWithContext(t, ctx, false, false, f, browserClient, "14=)=!(%)$/ZP()GHIÖ")
+			assert.Contains(t, res.Request.URL.String(), uiTS.URL+"/login-ts")
+			assert.NotEmpty(t, gjson.Get(body, "id").String(), "%s", body)
+			assert.Contains(t, gjson.Get(body, "ui.messages.0.text").String(), "invalid URL escape", "%s", body)
+		})
+
+		t.Run("type=spa", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+			f := testhelpers.InitializeLoginFlowViaBrowser(t, browserClient, publicTS, false, true, false, false, testhelpers.InitFlowWithContext(ctx))
+
+			body, res := testhelpers.LoginMakeRequestWithContext(t, ctx, false, true, f, browserClient, "14=)=!(%)$/ZP()GHIÖ")
+			assert.Contains(t, res.Request.URL.String(), publicTS.URL+login.RouteSubmitFlow)
+			assert.NotEmpty(t, gjson.Get(body, "id").String(), "%s", body)
+			assert.Contains(t, gjson.Get(body, "ui.messages.0.text").String(), "invalid URL escape", "%s", body)
+		})
+	})
+
+	t.Run("case=should fail because identifier first can not handle AAL2", func(t *testing.T) {
+		f := testhelpers.InitializeLoginFlowViaAPI(t, apiClient, publicTS, false)
+
+		update, err := reg.LoginFlowPersister().GetLoginFlow(context.Background(), uuid.FromStringOrNil(f.Id))
+		require.NoError(t, err)
+		update.RequestedAAL = identity.AuthenticatorAssuranceLevel2
+		require.NoError(t, reg.LoginFlowPersister().UpdateLoginFlow(context.Background(), update))
+
+		req, err := http.NewRequest("POST", f.Ui.Action, bytes.NewBufferString(`{"method":"identifier_first"}`))
+		require.NoError(t, err)
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Content-Type", "application/json")
+
+		actual, res := testhelpers.MockMakeAuthenticatedRequest(t, reg, conf, router.Router, req)
+		assert.Contains(t, res.Request.URL.String(), publicTS.URL+login.RouteSubmitFlow)
+		assert.Equal(t, text.NewErrorValidationLoginNoStrategyFound().Text, gjson.GetBytes(actual, "ui.messages.0.text").String())
+	})
+
+	t.Run("should return an error because the request does not exist", func(t *testing.T) {
+		check := func(t *testing.T, actual string) {
+			assert.Equal(t, int64(http.StatusNotFound), gjson.Get(actual, "code").Int(), "%s", actual)
+			assert.Equal(t, "Not Found", gjson.Get(actual, "status").String(), "%s", actual)
+			assert.Contains(t, gjson.Get(actual, "message").String(), "Unable to locate the resource", "%s", actual)
+		}
+
+		fakeFlow := &kratos.LoginFlow{
+			Ui: kratos.UiContainer{
+				Action: publicTS.URL + login.RouteSubmitFlow + "?flow=" + x.NewUUID().String(),
+			},
+		}
+
+		t.Run("type=api", func(t *testing.T) {
+			actual, res := testhelpers.LoginMakeRequestWithContext(t, ctx, true, false, fakeFlow, apiClient, "{}")
+			assert.Len(t, res.Cookies(), 0)
+			assert.Contains(t, res.Request.URL.String(), publicTS.URL+login.RouteSubmitFlow)
+			check(t, gjson.Get(actual, "error").Raw)
+		})
+
+		t.Run("type=browser", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+			actual, res := testhelpers.LoginMakeRequestWithContext(t, ctx, false, false, fakeFlow, browserClient, "")
+			assert.Contains(t, res.Request.URL.String(), errTS.URL)
+			check(t, actual)
+		})
+
+		t.Run("type=api", func(t *testing.T) {
+			actual, res := testhelpers.LoginMakeRequestWithContext(t, ctx, false, true, fakeFlow, apiClient, "{}")
+			assert.Len(t, res.Cookies(), 0)
+			assert.Contains(t, res.Request.URL.String(), publicTS.URL+login.RouteSubmitFlow)
+			check(t, gjson.Get(actual, "error").Raw)
+		})
+	})
+
+	t.Run("case=should return an error because the request is expired", func(t *testing.T) {
+		conf.MustSet(ctx, config.ViperKeySelfServiceLoginRequestLifespan, time.Millisecond*10)
+		conf.MustSet(ctx, config.ViperKeySecurityAccountEnumerationMitigate, true)
+		t.Cleanup(func() {
+			conf.MustSet(ctx, config.ViperKeySelfServiceLoginRequestLifespan, time.Hour)
+			conf.MustSet(ctx, config.ViperKeySecurityAccountEnumerationMitigate, nil)
+		})
+
+		values := url.Values{
+			"csrf_token": {x.FakeCSRFToken},
+			"identifier": {"identifier"},
+			"method":     {"identifier_first"},
+		}
+
+		t.Run("type=api", func(t *testing.T) {
+			f := testhelpers.InitializeLoginFlowViaAPIWithContext(t, ctx, apiClient, publicTS, false)
+
+			time.Sleep(time.Millisecond * 60)
+			actual, res := testhelpers.LoginMakeRequestWithContext(t, ctx, true, false, f, apiClient, testhelpers.EncodeFormAsJSON(t, true, values))
+			assert.Contains(t, res.Request.URL.String(), publicTS.URL+login.RouteSubmitFlow)
+			assert.NotEqual(t, "00000000-0000-0000-0000-000000000000", gjson.Get(actual, "use_flow_id").String())
+			assertx.EqualAsJSONExcept(t, flow.NewFlowExpiredError(time.Now()), json.RawMessage(actual), []string{"use_flow_id", "since", "expired_at"}, "expired", "%s", actual)
+		})
+
+		t.Run("type=browser", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+			f := testhelpers.InitializeLoginFlowViaBrowser(t, browserClient, publicTS, false, false, false, false)
+
+			time.Sleep(time.Millisecond * 60)
+			actual, res := testhelpers.LoginMakeRequestWithContext(t, ctx, false, false, f, browserClient, values.Encode())
+			assert.Contains(t, res.Request.URL.String(), uiTS.URL+"/login-ts")
+			assert.NotEqual(t, f.Id, gjson.Get(actual, "id").String(), "%s", actual)
+			assert.Contains(t, gjson.Get(actual, "ui.messages.0.text").String(), "expired", "%s", actual)
+		})
+
+		t.Run("type=SPA", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+			f := testhelpers.InitializeLoginFlowViaBrowser(t, browserClient, publicTS, false, true, false, false)
+
+			time.Sleep(time.Millisecond * 60)
+			actual, res := testhelpers.LoginMakeRequestWithContext(t, ctx, false, true, f, apiClient, testhelpers.EncodeFormAsJSON(t, true, values))
+			assert.Contains(t, res.Request.URL.String(), publicTS.URL+login.RouteSubmitFlow)
+			assert.NotEqual(t, "00000000-0000-0000-0000-000000000000", gjson.Get(actual, "use_flow_id").String())
+			assertx.EqualAsJSONExcept(t, flow.NewFlowExpiredError(time.Now()), json.RawMessage(actual), []string{"use_flow_id", "since", "expired_at"}, "expired", "%s", actual)
+		})
+	})
+
+	t.Run("case=should have correct CSRF behavior", func(t *testing.T) {
+		conf.MustSet(ctx, config.ViperKeySecurityAccountEnumerationMitigate, true)
+		t.Cleanup(func() {
+			conf.MustSet(ctx, config.ViperKeySecurityAccountEnumerationMitigate, nil)
+		})
+
+		values := url.Values{
+			"method":     {"identifier_first"},
+			"csrf_token": {"invalid_token"},
+			"identifier": {"login-identifier-csrf-browser"},
+		}
+
+		t.Run("case=should fail because of missing CSRF token/type=browser", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+			f := testhelpers.InitializeLoginFlowViaBrowser(t, browserClient, publicTS, false, false, false, false)
+
+			actual, res := testhelpers.LoginMakeRequest(t, false, false, f, browserClient, values.Encode())
+			assert.EqualValues(t, http.StatusOK, res.StatusCode)
+			assertx.EqualAsJSON(t, x.ErrInvalidCSRFToken,
+				json.RawMessage(actual), "%s", actual)
+		})
+
+		t.Run("case=should fail because of missing CSRF token/type=spa", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+			f := testhelpers.InitializeLoginFlowViaBrowser(t, browserClient, publicTS, false, true, false, false)
+
+			actual, res := testhelpers.LoginMakeRequest(t, false, true, f, browserClient, values.Encode())
+			assert.EqualValues(t, http.StatusForbidden, res.StatusCode)
+			assertx.EqualAsJSON(t, x.ErrInvalidCSRFToken,
+				json.RawMessage(gjson.Get(actual, "error").Raw), "%s", actual)
+		})
+
+		t.Run("case=should pass even without CSRF token/type=api", func(t *testing.T) {
+			f := testhelpers.InitializeLoginFlowViaAPI(t, apiClient, publicTS, false)
+
+			actual, res := testhelpers.LoginMakeRequest(t, true, false, f, apiClient, testhelpers.EncodeFormAsJSON(t, true, values))
+			assert.EqualValues(t, http.StatusBadRequest, res.StatusCode)
+			assert.Contains(t, actual, "1010022")
+		})
+
+		t.Run("case=should fail with correct CSRF error cause/type=api", func(t *testing.T) {
+			for k, tc := range []struct {
+				mod func(http.Header)
+				exp string
+			}{
+				{
+					mod: func(h http.Header) {
+						h.Add("Cookie", "name=bar")
+					},
+					exp: "The HTTP Request Header included the \\\"Cookie\\\" key",
+				},
+				{
+					mod: func(h http.Header) {
+						h.Add("Origin", "www.bar.com")
+					},
+					exp: "The HTTP Request Header included the \\\"Origin\\\" key",
+				},
+			} {
+				t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+					f := testhelpers.InitializeLoginFlowViaAPI(t, apiClient, publicTS, false)
+
+					req := testhelpers.NewRequest(t, true, "POST", f.Ui.Action, bytes.NewBufferString(testhelpers.EncodeFormAsJSON(t, true, values)))
+					tc.mod(req.Header)
+
+					res, err := apiClient.Do(req)
+					require.NoError(t, err)
+					defer res.Body.Close()
+
+					actual := string(ioutilx.MustReadAll(res.Body))
+					assert.EqualValues(t, http.StatusBadRequest, res.StatusCode)
+					assert.Contains(t, actual, tc.exp)
+				})
+			}
+		})
+	})
+
+	expectValidationError := func(t *testing.T, isAPI, refresh, isSPA bool, values func(url.Values)) string {
+		return testhelpers.SubmitLoginForm(t, isAPI, nil, publicTS, values,
+			isSPA, refresh,
+			testhelpers.ExpectStatusCode(isAPI || isSPA, http.StatusBadRequest, http.StatusOK),
+			testhelpers.ExpectURL(isAPI || isSPA, publicTS.URL+login.RouteSubmitFlow, conf.SelfServiceFlowLoginUI(ctx).String()))
+	}
+
+	t.Run("should return an error because the credentials are invalid (user does not exist)", func(t *testing.T) {
+		conf.MustSet(ctx, config.ViperKeySecurityAccountEnumerationMitigate, false)
+		t.Cleanup(func() {
+			conf.MustSet(ctx, config.ViperKeySecurityAccountEnumerationMitigate, nil)
+		})
+
+		check := func(t *testing.T, body string, start time.Time) {
+			assert.NotEmpty(t, gjson.Get(body, "id").String(), "%s", body)
+			assert.Contains(t, gjson.Get(body, "ui.action").String(), publicTS.URL+login.RouteSubmitFlow, "%s", body)
+			assert.Contains(t, body, text.NewErrorValidationAccountNotFound().Text)
+		}
+
+		values := func(v url.Values) {
+			v.Set("identifier", "identifier")
+			v.Set("method", "identifier_first")
+		}
+
+		t.Run("type=browser", func(t *testing.T) {
+			start := time.Now()
+			check(t, expectValidationError(t, false, false, false, values), start)
+		})
+
+		t.Run("type=SPA", func(t *testing.T) {
+			start := time.Now()
+			check(t, expectValidationError(t, false, false, true, values), start)
+		})
+
+		t.Run("type=api", func(t *testing.T) {
+			start := time.Now()
+			check(t, expectValidationError(t, true, false, false, values), start)
+		})
+	})
+
+	t.Run("should pass with real request", func(t *testing.T) {
+		identifier, pwd := x.NewUUID().String(), "password"
+		createIdentity(ctx, reg, t, identifier, pwd)
+
+		firstValues := func(v url.Values) {
+			v.Set("identifier", identifier)
+			v.Set("method", "identifier_first")
+		}
+
+		secondValues := func(v url.Values) {
+			v.Set("identifier", identifier)
+			v.Set("password", pwd)
+			v.Set("method", "password")
+		}
+
+		t.Run("type=browser", func(t *testing.T) {
+			browserClient := testhelpers.NewClientWithCookies(t)
+
+			secondStep := testhelpers.SubmitLoginForm(t, false, browserClient, publicTS, firstValues,
+				true, false, http.StatusBadRequest, publicTS.URL+login.RouteSubmitFlow)
+			t.Logf("secondStep: %s", secondStep)
+			assert.Contains(t, secondStep, "current-password")
+			assert.Contains(t, secondStep, `"value":"password"`)
+
+			body := testhelpers.SubmitLoginForm(t, false, browserClient, publicTS, secondValues,
+				false, false, http.StatusOK, redirTS.URL)
+
+			assert.Equal(t, identifier, gjson.Get(body, "identity.traits.subject").String(), "%s", body)
+		})
+
+		t.Run("type=spa", func(t *testing.T) {
+			hc := testhelpers.NewClientWithCookies(t)
+
+			secondStep := testhelpers.SubmitLoginForm(t, false, hc, publicTS, firstValues,
+				true, false, http.StatusBadRequest, publicTS.URL+login.RouteSubmitFlow)
+			t.Logf("secondStep: %s", secondStep)
+			assert.Contains(t, secondStep, "current-password")
+			assert.Contains(t, secondStep, `"value":"password"`)
+
+			body := testhelpers.SubmitLoginForm(t, false, hc, publicTS, secondValues,
+				true, false, http.StatusOK, publicTS.URL+login.RouteSubmitFlow)
+
+			assert.Equal(t, identifier, gjson.Get(body, "session.identity.traits.subject").String(), "%s", body)
+			assert.Empty(t, gjson.Get(body, "session_token").String(), "%s", body)
+			assert.Empty(t, gjson.Get(body, "session.token").String(), "%s", body)
+
+			// Was the session cookie set?
+			require.NotEmpty(t, hc.Jar.Cookies(urlx.ParseOrPanic(publicTS.URL)), "%+v", hc.Jar)
+		})
+
+		t.Run("type=api", func(t *testing.T) {
+			secondStep := testhelpers.SubmitLoginForm(t, true, nil, publicTS, firstValues,
+				false, false, http.StatusBadRequest, publicTS.URL+login.RouteSubmitFlow)
+			t.Logf("secondStep: %s", secondStep)
+			assert.Contains(t, secondStep, "current-password")
+			assert.Contains(t, secondStep, `"value":"password"`)
+
+			body := testhelpers.SubmitLoginForm(t, true, nil, publicTS, secondValues,
+				false, false, http.StatusOK, publicTS.URL+login.RouteSubmitFlow)
+
+			assert.Equal(t, identifier, gjson.Get(body, "session.identity.traits.subject").String(), "%s", body)
+			st := gjson.Get(body, "session_token").String()
+			assert.NotEmpty(t, st, "%s", body)
+		})
+	})
+}
 
 func TestFormHydration(t *testing.T) {
 	ctx := context.Background()
