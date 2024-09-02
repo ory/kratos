e56f142f1b126b17ab1e23fdfbf2ba936294210c
Arne Luenser <arne.luenser@ory.sh>
2024-09-02 18:31:57 +0200

    fix: assertion

--- selfservice/strategy/oidc/strategy_test.go
+++ selfservice/strategy/oidc/strategy_test.go
@@ -774,10 +774,7 @@ func TestStrategy(t *testing.T) {
 			require.NoError(t, err)
 			require.NoError(t, res.Body.Close())
 
-			// The reason for `invalid_client` here is that the code was already used and the session was already authenticated. The invalid_client
-			// happens because of the way Golang's OAuth2 library is trying out different auth methods when a token request fails, which obfuscates
-			// the underlying error.
-			assert.Contains(t, string(body), "invalid_client", "%s", body)
+			assertSystemErrorWithMessage(t, res, body, http.StatusInternalServerError, "The authorization code has already been used.")
 		})
 	})
 
