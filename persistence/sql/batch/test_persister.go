e4913293737713fe49060d0f24233be2565222c0
Henning Perl <henning.perl@ory.sh>
2024-09-13 12:58:48 +0200

    add batch persister test

---
+++ persistence/sql/batch/test_persister.go
@@ -0,0 +1,90 @@
+package batch
+
+import (
+	"context"
+	"testing"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ory/kratos/identity"
+	"github.com/ory/kratos/persistence"
+	"github.com/ory/x/dbal"
+	"github.com/ory/x/otelx"
+	"github.com/ory/x/sqlcon"
+)
+
+func TestPersister(ctx context.Context, tracer *otelx.Tracer, p persistence.Persister) func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Run("method=batch.Create", func(t *testing.T) {
+
+			ident1 := identity.NewIdentity("")
+			ident1.NID = p.NetworkID(ctx)
+			ident2 := identity.NewIdentity("")
+			ident2.NID = p.NetworkID(ctx)
+
+			// Create two identities
+			_ = p.Transaction(ctx, func(ctx context.Context, tx *pop.Connection) error {
+				conn := &TracerConnection{
+					Tracer:     tracer,
+					Connection: tx,
+				}
+
+				err := Create(ctx, conn, []*identity.Identity{ident1, ident2})
+				require.NoError(t, err)
+
+				return nil
+			})
+
+			require.NotEqual(t, uuid.Nil, ident1.ID)
+			require.NotEqual(t, uuid.Nil, ident2.ID)
+
+			// Create conflicting verifiable addresses
+			_ = p.Transaction(ctx, func(ctx context.Context, tx *pop.Connection) error {
+				conn := &TracerConnection{
+					Tracer:     tracer,
+					Connection: tx,
+				}
+
+				err := Create(ctx, conn, []*identity.VerifiableAddress{{
+					Value:      "foo.1@bar.de",
+					IdentityID: ident1.ID,
+					NID:        ident1.NID,
+				}, {
+					Value:      "foo.2@bar.de",
+					IdentityID: ident1.ID,
+					NID:        ident1.NID,
+				}, {
+					Value:      "conflict@bar.de",
+					IdentityID: ident1.ID,
+					NID:        ident1.NID,
+				}, {
+					Value:      "foo.3@bar.de",
+					IdentityID: ident1.ID,
+					NID:        ident1.NID,
+				}, {
+					Value:      "conflict@bar.de",
+					IdentityID: ident1.ID,
+					NID:        ident1.NID,
+				}, {
+					Value:      "foo.4@bar.de",
+					IdentityID: ident1.ID,
+					NID:        ident1.NID,
+				}})
+
+				assert.ErrorIs(t, err, sqlcon.ErrUniqueViolation)
+
+				if conn.Connection.Dialect.Name() != dbal.DriverMySQL {
+					// MySQL does not support partial errors.
+					partialErr := new(PartialConflictError[identity.VerifiableAddress])
+					require.ErrorAs(t, err, &partialErr)
+					assert.Len(t, partialErr.Failed, 1)
+				}
+
+				return nil
+			})
+		})
+	}
+}
