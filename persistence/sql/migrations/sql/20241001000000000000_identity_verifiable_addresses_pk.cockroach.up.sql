6220240e2fc9113917aff2d10a999cf41d17333e
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-02 18:26:02 +0200

    chore: synchronize workspaces

---
+++ persistence/sql/migrations/sql/20241001000000000000_identity_verifiable_addresses_pk.cockroach.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE identity_verifiable_addresses ALTER PRIMARY KEY USING COLUMNS (identity_id,id);
---
+++ persistence/sql/migrations/sql/20241001000000000001_identity_recovery_addresses_pk.cockroach.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE identity_recovery_addresses ALTER PRIMARY KEY USING COLUMNS (id);
