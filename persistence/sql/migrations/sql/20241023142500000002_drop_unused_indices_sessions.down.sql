12ff9b2cde89b2c065a60e176d4b0489cf2d2e39
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-24 12:00:14 +0200

    fix: remove total count from listSessions and improve indices
    This patch changes sorting to improve performance on list session endpoints. It also removes the `x-total-count` header from list responses.
    
    BREAKING CHANGE: The total count header `x-total-count` will no longer be sent in response to `GET /admin/sessions` requests.
    
    Closes https://github.com/ory-corp/cloud/issues/7177

---
+++ persistence/sql/migrations/sql/20241023142500000002_drop_unused_indices_sessions.down.sql
@@ -0,0 +1,8 @@
+CREATE INDEX sessions_nid_id_identity_id_idx ON sessions (nid ASC, identity_id ASC, id ASC);
+CREATE INDEX sessions_id_nid_idx ON sessions  (id ASC, nid ASC);
+CREATE INDEX sessions_token_nid_idx ON sessions (nid ASC, token ASC);
+CREATE INDEX sessions_identity_id_nid_sorted_idx ON sessions (identity_id ASC, nid ASC, authenticated_at DESC);
+CREATE INDEX sessions_nid_created_at_id_idx ON sessions (nid ASC, created_at DESC, id ASC);
+
+DROP INDEX sessions_nid_expires_at_authenticated_at_idx;
+DROP INDEX sessions_identity_id_idx;
