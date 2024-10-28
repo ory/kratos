745bcbb68689da0b8323c8ca9063ebaedd414af8
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-28 16:53:27 +0100

    chore: synchronize workspaces

--- persistence/sql/migrations/sql/20241023142500000001_drop_unused_indices_identity_credentials.mysql.up.sql
+++ persistence/sql/migrations/sql/20241023142500000001_drop_unused_indices_identity_credentials.mysql.up.sql
@@ -1,5 +1,6 @@
-CREATE INDEX identity_credentials_nid_identity_id_v2_idx ON identity_credentials (nid ASC, identity_id ASC);
+CREATE INDEX identity_credentials_identity_id_idx ON identity_credentials (identity_id ASC);
+CREATE INDEX identity_credentials_nid_idx ON identity_credentials (nid ASC);
 
-DROP INDEX identity_credentials_nid_id_idx ON identity_credentials;
 DROP INDEX identity_credentials_id_nid_idx ON identity_credentials;
+DROP INDEX identity_credentials_nid_id_idx ON identity_credentials;
 DROP INDEX identity_credentials_nid_identity_id_idx ON identity_credentials;
--- persistence/sql/migrations/sql/20241023142500000003_drop_unused_indices_credential_identifiers.down.sql
+++ persistence/sql/migrations/sql/20241023142500000003_drop_unused_indices_credential_identifiers.down.sql
@@ -1,6 +1,5 @@
-DROP INDEX identity_credential_identifiers_nid_i_ici_v2_idx;
+CREATE INDEX IF NOT EXISTS identity_credential_identifiers_nid_id_idx ON identity_credential_identifiers (nid ASC, id ASC);
+CREATE INDEX IF NOT EXISTS identity_credential_identifiers_id_nid_idx ON identity_credential_identifiers (id ASC, nid ASC);
+CREATE INDEX IF NOT EXISTS identity_credential_identifiers_nid_identity_credential_id_idx ON identity_credential_identifiers (identity_credential_id ASC, nid ASC);
 
-CREATE INDEX identity_credential_identifiers_nid_identity_credential_id_idx ON identity_credential_identifiers (identity_credential_id ASC, nid ASC);
-CREATE INDEX identity_credential_identifiers_nid_i_ici_idx ON identity_credential_identifiers (nid ASC, identifier ASC, identity_credential_id ASC);
-CREATE INDEX identity_credential_identifiers_nid_id_idx ON identity_credential_identifiers (nid ASC, id ASC);
-CREATE INDEX identity_credential_identifiers_id_nid_idx ON identity_credential_identifiers (id ASC, nid ASC);
+DROP INDEX IF EXISTS identity_credential_identifiers_identity_credential_id_idx;
