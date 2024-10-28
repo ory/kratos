f35f0cdc046e68205216cc56c319f214e9189f25
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-28 13:52:04 +0100

    chore: synchronize workspaces

--- persistence/sql/migrations/sql/20241023142500000002_drop_unused_indices_sessions.mysql.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX sessions_nid_expires_at_authenticated_at_idx ON sessions (nid ASC, expires_at ASC, authenticated_at DESC); -- Used for listing sessions
-CREATE INDEX sessions_identity_id_idx ON sessions (identity_at DESC); -- Used for listing sessions
-
-DROP INDEX sessions_nid_id_identity_id_idx ON sessions;
-DROP INDEX sessions_id_nid_idx ON sessions;
-DROP INDEX sessions_token_nid_idx ON sessions;
-DROP INDEX sessions_identity_id_nid_sorted_idx ON sessions;
-DROP INDEX sessions_nid_created_at_id_idx ON sessions;
