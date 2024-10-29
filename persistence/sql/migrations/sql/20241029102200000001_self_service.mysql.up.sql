352577db63277fef605900e57402661d5b95a414
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-29 11:27:08 +0100

    chore: synchronize workspaces

--- persistence/sql/migrations/sql/20241029102200000001_self_service.mysql.up.sql
+++ persistence/sql/migrations/sql/20241029102200000001_self_service.mysql.up.sql
@@ -1,26 +1,26 @@
-CREATE INDEX IF NOT EXISTS selfservice_login_flows_nid_idx ON selfservice_login_flows (nid ASC);
-DROP INDEX IF EXISTS selfservice_login_flows_nid_id_idx ON selfservice_login_flows;
-DROP INDEX IF EXISTS selfservice_login_flows_id_nid_idx ON selfservice_login_flows;
+CREATE INDEX selfservice_login_flows_nid_idx ON selfservice_login_flows (nid ASC);
+DROP INDEX selfservice_login_flows_nid_id_idx ON selfservice_login_flows;
+DROP INDEX selfservice_login_flows_id_nid_idx ON selfservice_login_flows;
 
-CREATE INDEX IF NOT EXISTS selfservice_errors_nid_idx ON selfservice_errors (nid ASC);
-DROP INDEX IF EXISTS selfservice_errors_errors_nid_id_idx;
+CREATE INDEX selfservice_errors_nid_idx ON selfservice_errors (nid ASC);
+DROP INDEX selfservice_errors_errors_nid_id_idx;
 
-CREATE INDEX IF NOT EXISTS selfservice_recovery_flows_nid_idx ON selfservice_recovery_flows (nid ASC);
-CREATE INDEX IF NOT EXISTS selfservice_recovery_flows_recovered_identity_id_idx ON selfservice_recovery_flows (recovered_identity_id ASC);
-DROP INDEX IF EXISTS selfservice_recovery_flows_nid_id_idx ON selfservice_recovery_flows;
-DROP INDEX IF EXISTS selfservice_recovery_flows_id_nid_idx ON selfservice_recovery_flows;
-DROP INDEX IF EXISTS selfservice_recovery_flows_recovered_identity_id_nid_idx ON selfservice_recovery_flows;
+CREATE INDEX selfservice_recovery_flows_nid_idx ON selfservice_recovery_flows (nid ASC);
+CREATE INDEX selfservice_recovery_flows_recovered_identity_id_idx ON selfservice_recovery_flows (recovered_identity_id ASC);
+DROP INDEX selfservice_recovery_flows_nid_id_idx ON selfservice_recovery_flows;
+DROP INDEX selfservice_recovery_flows_id_nid_idx ON selfservice_recovery_flows;
+DROP INDEX selfservice_recovery_flows_recovered_identity_id_nid_idx ON selfservice_recovery_flows;
 
-CREATE INDEX IF NOT EXISTS selfservice_registration_flows_nid_idx ON selfservice_registration_flows (nid ASC);
-DROP INDEX IF EXISTS selfservice_registration_flows_nid_id_idx ON selfservice_registration_flows;
-DROP INDEX IF EXISTS selfservice_registration_flows_id_nid_idx ON selfservice_registration_flows;
+CREATE INDEX selfservice_registration_flows_nid_idx ON selfservice_registration_flows (nid ASC);
+DROP INDEX selfservice_registration_flows_nid_id_idx ON selfservice_registration_flows;
+DROP INDEX selfservice_registration_flows_id_nid_idx ON selfservice_registration_flows;
 
-CREATE INDEX IF NOT EXISTS selfservice_settings_flows_nid_idx ON selfservice_settings_flows (nid ASC);
-CREATE INDEX IF NOT EXISTS selfservice_settings_flows_identity_id_idx ON selfservice_settings_flows (identity_id ASC);
-DROP INDEX IF EXISTS selfservice_settings_flows_nid_id_idx ON selfservice_settings_flows;
-DROP INDEX IF EXISTS selfservice_settings_flows_id_nid_idx ON selfservice_settings_flows;
-DROP INDEX IF EXISTS selfservice_settings_flows_identity_id_nid_idx ON selfservice_settings_flows;
+CREATE INDEX selfservice_settings_flows_nid_idx ON selfservice_settings_flows (nid ASC);
+CREATE INDEX selfservice_settings_flows_identity_id_idx ON selfservice_settings_flows (identity_id ASC);
+DROP INDEX selfservice_settings_flows_nid_id_idx ON selfservice_settings_flows;
+DROP INDEX selfservice_settings_flows_id_nid_idx ON selfservice_settings_flows;
+DROP INDEX selfservice_settings_flows_identity_id_nid_idx ON selfservice_settings_flows;
 
-CREATE INDEX IF NOT EXISTS selfservice_verification_flows_nid_idx ON selfservice_verification_flows (nid ASC);
-DROP INDEX IF EXISTS selfservice_verification_flows_nid_id_idx ON selfservice_verification_flows;
-DROP INDEX IF EXISTS selfservice_verification_flows_id_nid_idx ON selfservice_verification_flows;
+CREATE INDEX selfservice_verification_flows_nid_idx ON selfservice_verification_flows (nid ASC);
+DROP INDEX selfservice_verification_flows_nid_id_idx ON selfservice_verification_flows;
+DROP INDEX selfservice_verification_flows_id_nid_idx ON selfservice_verification_flows;
