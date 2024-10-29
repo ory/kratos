2a3d04709a20422058664f20c9602286fa07ba75
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-29 09:34:00 +0100

    chore: synchronize workspaces

--- persistence/sql/migrations/sql/20241023142500000001_drop_unused_indices_identity_credentials.mysql.up.sql
+++ persistence/sql/migrations/sql/20241023142500000001_drop_unused_indices_identity_credentials.mysql.up.sql
@@ -1,6 +1,5 @@
-CREATE INDEX identity_credentials_identity_id_idx ON identity_credentials (identity_id ASC);
 CREATE INDEX identity_credentials_nid_idx ON identity_credentials (nid ASC);
 
 DROP INDEX identity_credentials_id_nid_idx ON identity_credentials;
 DROP INDEX identity_credentials_nid_id_idx ON identity_credentials;
-DROP INDEX identity_credentials_nid_identity_id_idx ON identity_credentials;
+
--- persistence/sql/migrations/sql/20241023142500000002_drop_unused_indices_sessions.up.sql
+++ persistence/sql/migrations/sql/20241023142500000002_drop_unused_indices_sessions.up.sql
@@ -1,6 +1,6 @@
 CREATE INDEX IF NOT EXISTS sessions_list_idx ON sessions (nid ASC, created_at DESC, id ASC);
 CREATE INDEX IF NOT EXISTS sessions_list_active_idx ON sessions (nid ASC, expires_at ASC, active ASC, created_at DESC, id ASC);
-CREATE INDEX IF NOT EXISTS sessions_list_identity_idx ON sessions (nid ASC, identity_id ASC, created_at DESC);
+CREATE INDEX IF NOT EXISTS sessions_list_identity_idx ON sessions (identity_id ASC, nid ASC, created_at DESC);
 
 DROP INDEX IF EXISTS sessions_nid_id_identity_id_idx;
 DROP INDEX IF EXISTS sessions_id_nid_idx;
