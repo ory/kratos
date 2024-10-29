c5a0da416c6ed3df9dae09558d6526861907a745
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-29 10:21:36 +0100

    chore: synchronize workspaces

--- persistence/sql/migrations/sql/20241023142500000003_drop_unused_indices_credential_identifiers.cockroach.down.sql
+++ persistence/sql/migrations/sql/20241023142500000003_drop_unused_indices_credential_identifiers.cockroach.down.sql
@@ -4,8 +4,8 @@ CREATE UNIQUE INDEX IF NOT EXISTS identity_credential_identifiers_identifier_nid
 DROP INDEX IF EXISTS identity_credential_identifiers_identifier_nid_type_uq_idx_deleteme;
 --
 
-CREATE INDEX IF NOT EXISTS identity_credential_identifiers_nid_id_idx ON identity_credentials_identifiers (nid ASC, id ASC);
-CREATE INDEX IF NOT EXISTS identity_credential_identifiers_id_nid_idx ON identity_credentials_identifiers (id ASC, nid ASC);
-CREATE INDEX IF NOT EXISTS identity_credential_identifiers_nid_identity_credential_id_idx ON identity_credentials_identifiers (identity_credential_id ASC, nid ASC);
+CREATE INDEX IF NOT EXISTS identity_credential_identifiers_nid_id_idx ON identity_credential_identifiers (nid ASC, id ASC);
+CREATE INDEX IF NOT EXISTS identity_credential_identifiers_id_nid_idx ON identity_credential_identifiers (id ASC, nid ASC);
+CREATE INDEX IF NOT EXISTS identity_credential_identifiers_nid_identity_credential_id_idx ON identity_credential_identifiers (identity_credential_id ASC, nid ASC);
 
 DROP INDEX IF EXISTS identity_credential_identifiers_identity_credential_id_idx;
