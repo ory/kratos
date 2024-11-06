e88bb4d181007b4cdd08fd8d3bf67fe3b2f5f71d
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-11-06 13:29:18 +0100

    chore: synchronize workspaces

--- selfservice/flow/settings/hook_test.go
+++ selfservice/flow/settings/hook_test.go
@@ -73,7 +73,7 @@ func TestSettingsExecutor(t *testing.T) {
 					a.RequestURL = x.RequestURL(r).String()
 					require.NoError(t, reg.SettingsFlowPersister().CreateSettingsFlow(r.Context(), a))
 					_ = handleErr(t, w, r, reg.SettingsHookExecutor().
-						PostSettingsHook(w, r, strategy, &settings.UpdateContext{Flow: a, Session: sess}, i))
+						PostSettingsHook(ctx, w, r, strategy, &settings.UpdateContext{Flow: a, Session: sess}, i))
 				})
 				ts := httptest.NewServer(router)
 				t.Cleanup(ts.Close)
