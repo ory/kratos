9d137f8d12b55a97eb09de05878afc67524cf5c9
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-11-06 15:42:36 +0100

    chore: fix test

--- selfservice/strategy/link/strategy_verification_test.go
+++ selfservice/strategy/link/strategy_verification_test.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -143,7 +144,7 @@ func TestVerification(t *testing.T) {
 	t.Run("description=should require a valid email to be sent", func(t *testing.T) {
 		check := func(t *testing.T, actual string, value string) {
 			assert.EqualValues(t, string(node.LinkGroup), gjson.Get(actual, "active").String(), "%s", actual)
-			assert.EqualValues(t, fmt.Sprintf("%s is not a valid email address", value),
+			assert.EqualValues(t, strings.ReplaceAll(fmt.Sprintf("%q is not a valid email address", value), "\"", ""),
 				gjson.Get(actual, "ui.nodes.#(attributes.name==email).messages.0.text").String(),
 				"%s", actual)
 		}
