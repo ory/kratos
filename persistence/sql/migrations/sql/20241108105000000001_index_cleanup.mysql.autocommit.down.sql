1008639428a6b72e0aa47bd13fe9c1d120aafb6e
hackerman <3372410+aeneasr@users.noreply.github.com>
2024-11-12 10:52:39 +0100

    feat: drop unused indices post index migration (#4201)

---
+++ persistence/sql/migrations/sql/20241108105000000001_index_cleanup.mysql.autocommit.down.sql
@@ -0,0 +1,5 @@
+CREATE INDEX identity_credential_identifiers_nid_ici_i_idx
+  ON identity_credential_identifiers (nid ASC, identity_credential_id ASC, identifier ASC);
+
+CREATE INDEX identity_credential_identifiers_identity_credential_id_idx
+  ON identity_credential_identifiers (identity_credential_id ASC);
---
+++ persistence/sql/migrations/sql/20241108105000000001_index_cleanup.mysql.autocommit.up.sql
@@ -0,0 +1,5 @@
+-- This index is replaced by identity_credential_identifiers_ici_nid_i_idx (included in the previous OEL release)
+DROP INDEX identity_credential_identifiers_nid_ici_i_idx ON identity_credential_identifiers;
+
+-- This index is replaced by identity_credential_identifiers_ici_nid_i_idx (included in the previous OEL release)
+DROP INDEX identity_credential_identifiers_identity_credential_id_idx ON identity_credential_identifiers;
