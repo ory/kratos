2c461fa7fb5616a159b995c5f0c6358f13d757a3
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-04-07 12:32:08 +0200

    chore: synchronize workspaces

--- selfservice/flow/login/extension_identifier_label_test.go
+++ selfservice/flow/login/extension_identifier_label_test.go
@@ -40,11 +40,16 @@ func constructSchema(t *testing.T, ecModifier, ucModifier func(*schema.Extension
 	require.NoError(t, err)
 	ec, err = sjson.DeleteBytes(ec, "credentials.code.via")
 	require.NoError(t, err)
+	ec, err = sjson.DeleteBytes(ec, "organization.matcher")
+	require.NoError(t, err)
+
 	uc, err = sjson.DeleteBytes(uc, "verification")
 	require.NoError(t, err)
 	uc, err = sjson.DeleteBytes(uc, "recovery")
 	require.NoError(t, err)
 	uc, err = sjson.DeleteBytes(uc, "credentials.code.via")
+	require.NoError(t, err)
+	uc, err = sjson.DeleteBytes(uc, "organization.matcher")
 	require.NoError(t, err)
 
 	return "base64://" + base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf(`
