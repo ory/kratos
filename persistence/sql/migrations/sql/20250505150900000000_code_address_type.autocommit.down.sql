170f462ed58ca53d32910be7d465b375a1e0d8f6
hackerman <3372410+aeneasr@users.noreply.github.com>
2025-05-12 10:27:11 +0200

    Update persistence/sql/migrations/sql/20250505150900000000_code_address_type.autocommit.down.sql

--- persistence/sql/migrations/sql/20250505150900000000_code_address_type.autocommit.down.sql
+++ persistence/sql/migrations/sql/20250505150900000000_code_address_type.autocommit.down.sql
@@ -1,2 +1,2 @@
-ALTER TABLE identity_login_codes ALTER COLUMN address_type TYPE CHAR(36);
+ALTER TABLE identity_login_codes ALTER COLUMN address_type TYPE VARCHAR(36);
 ALTER TABLE identity_registration_codes ALTER COLUMN address_type TYPE CHAR(36);
