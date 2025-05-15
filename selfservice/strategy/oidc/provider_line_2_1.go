292f65d6bd1bc70b2f13b92bdbcb8e30256e0a17
Arne Luenser <arne.luenser@ory.sh>
2025-05-15 17:22:50 +0200

    fix: add default issuer URL for LINE (#4415)
    Fixed+expanded relevant comment.
    
    Fixed some tracing issues.
    
    Added error info and missing res.Body.Close() in courier.
    
    ---------
    
    Co-authored-by: ory-bot <60093411+ory-bot@users.noreply.github.com>

--- selfservice/strategy/oidc/provider_line_2_1.go
+++ selfservice/strategy/oidc/provider_line_2_1.go
@@ -17,6 +17,7 @@ func NewProviderLineV21(
 	config *Configuration,
 	reg Dependencies,
 ) Provider {
+	config.IssuerURL = "https://access.line.me"
 	return &ProviderLineV21{
 		&ProviderGenericOIDC{
 			config: config,
@@ -27,15 +28,12 @@ func NewProviderLineV21(
 
 func (g *ProviderLineV21) Exchange(ctx context.Context, code string, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
 	o, err := g.ProviderGenericOIDC.OAuth2(ctx)
-
 	if err != nil {
 		return nil, err
 	}
-	// Line login requires adding id_token_key_type=JWK when getting the token in order to issue an HS256 token.
+
+	// Line login requires adding id_token_key_type=JWK when getting the token in order to issue an ES256 token.
+	// https://blog.miniasp.com/post/2022/04/08/LINE-Login-with-OpenID-Connect-in-ASPNET-Core (Chinese)
 	opts = append(opts, oauth2.SetAuthURLParam("id_token_key_type", "JWK"))
-
-	token, err := o.Exchange(ctx, code, opts...)
-
-	return token, err
-
+	return o.Exchange(ctx, code, opts...)
 }
