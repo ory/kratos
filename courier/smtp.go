01e1dd04dce4ca08ba8a855866f47d635ed68076
Philippe Gaultier <philigaultier@gmail.com>
2025-11-11 09:12:06 +0100

    fix: fix flaky email test
    GitOrigin-RevId: feab815802aa1088bceca0ea7a968d99f674e657

--- courier/smtp.go
+++ courier/smtp.go
@@ -99,25 +99,25 @@ func NewSMTPClient(deps Dependencies, cfg *config.SMTPConfig) (*SMTPClient, erro
 func (c *courier) QueueEmail(ctx context.Context, t EmailTemplate) (uuid.UUID, error) {
 	recipient, err := t.EmailRecipient()
 	if err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, errors.WithStack(err)
 	}
 	if _, err := mail.ParseAddress(recipient); err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, errors.WithStack(err)
 	}
 
 	subject, err := t.EmailSubject(ctx)
 	if err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, errors.WithStack(err)
 	}
 
 	bodyPlaintext, err := t.EmailBodyPlaintext(ctx)
 	if err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, errors.WithStack(err)
 	}
 
 	templateData, err := json.Marshal(t)
 	if err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, errors.WithStack(err)
 	}
 
 	message := &Message{
@@ -132,7 +132,7 @@ func (c *courier) QueueEmail(ctx context.Context, t EmailTemplate) (uuid.UUID, e
 	}
 
 	if err := c.deps.CourierPersister().AddMessage(ctx, message); err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, errors.WithStack(err)
 	}
 
 	return message.ID, nil
