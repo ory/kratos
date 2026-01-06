ecf73dce9022dce6b3b0fda8b304e95d77986110
aeneasr <3372410+aeneasr@users.noreply.github.com>
2026-01-06 12:23:13 +0000

    autogen(sdk): bump to 4c3e8f5aa769533cc0208b700c01b3241b6fa41b
    GitOrigin-RevId: 274a724fa0fbe035d34f2c4cea4f8aff327ec8c8

--- selfservice/hook/require_verified_address.go
+++ selfservice/hook/require_verified_address.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gofrs/uuid"
+
 	"github.com/ory/kratos/driver/config"
 	"github.com/ory/kratos/selfservice/flow"
 	"github.com/ory/kratos/selfservice/flow/verification"
